1dcce2517b4f50d18856353f5b3b93e43c0812e8
Anubhav Jain <ajain@lbl.gov>
2016-02-08 16:20:41 -0800

    fix unit test

--- matmethods/vasp/firetasks/tests/test_setup.py
+++ matmethods/vasp/firetasks/tests/test_setup.py
@@ -19,10 +19,10 @@ class SetupTests(unittest.TestCase):
                                 [0.00, -2.2171384943, 3.1355090603]])
         cls.struct_si = IStructure(lattice, ["Si"] * 2, coords)
 
-        cls.ref_incar = Incar.from_file(os.path.join(module_dir, "reference_files", "INCAR"))
-        cls.ref_poscar = Poscar.from_file(os.path.join(module_dir, "reference_files", "POSCAR"))
-        cls.ref_potcar = Potcar.from_file(os.path.join(module_dir, "reference_files", "POTCAR"))
-        cls.ref_kpoints = Kpoints.from_file(os.path.join(module_dir, "reference_files", "KPOINTS"))
+        cls.ref_incar = Incar.from_file(os.path.join(module_dir, "reference_files", "setup_test", "INCAR"))
+        cls.ref_poscar = Poscar.from_file(os.path.join(module_dir, "reference_files", "setup_test", "POSCAR"))
+        cls.ref_potcar = Potcar.from_file(os.path.join(module_dir, "reference_files", "setup_test", "POTCAR"))
+        cls.ref_kpoints = Kpoints.from_file(os.path.join(module_dir, "reference_files", "setup_test", "KPOINTS"))
 
 
     def tearDown(self):
