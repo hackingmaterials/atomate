2d614e034d962926919bb34134df2dd88f3692b2
Anubhav Jain <ajain@lbl.gov>
2016-04-12 12:50:31 -0700

    incr small gap multiplier

--- matmethods/vasp/workflows/automatic/standard.py
+++ matmethods/vasp/workflows/automatic/standard.py
@@ -22,7 +22,7 @@ def wf_band_structure(structure):
     wf = use_custodian(wf, fw_name_constraint="structure optimization",
                        custodian_params={"job_type": "double_relaxation_run"})
     wf = decorate_write_name(wf)
-    wf = add_small_gap_multiplier(wf, 0.5, 5, "static")
+    wf = add_small_gap_multiplier(wf, 0.5, 10, "static")
     wf = add_small_gap_multiplier(wf, 0.5, 10, "nscf")
     wf = use_scratch_dir(wf, ">>scratch_dir<<")
 
