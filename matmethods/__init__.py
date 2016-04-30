1bd742c119dceca1c7d31e55ab750d04e5bda927
__sp__ <ongsp@eng.ucsd.edu>
2016-04-30 06:59:29 -0700

    Move spec parsing code to root matmethods. Add mmwf script.

--- matmethods/__init__.py
+++ matmethods/__init__.py
@@ -1,1 +1,19 @@
 __version__ = '0.0.2'
+
+
+from monty.json import MontyDecoder
+from fireworks import Workflow, LaunchPad
+
+
+def get_wf_from_spec_dict(structure, wfspec):
+    fws = []
+    for d in wfspec["fireworks"]:
+        modname, classname = d["fw"].rsplit(".", 1)
+        mod = __import__(modname, globals(), locals(), [classname], 0)
+        if hasattr(mod, classname):
+            cls_ = getattr(mod, classname)
+            kwargs = {k: MontyDecoder().process_decoded(v) for k, v in d.get("params", {}).items()}
+            if "parents" in kwargs:
+                kwargs["parents"] = fws[kwargs["parents"]]
+            fws.append(cls_(structure, **kwargs))
+    return Workflow(fws, name=structure.composition.reduced_formula)
--- matmethods/vasp/workflows/base/band_structure.py
+++ matmethods/vasp/workflows/base/band_structure.py
@@ -14,7 +14,6 @@ from matmethods.vasp.fws import OptimizeFW, StaticFW, NonSCFUniformFW, \
     NonSCFLineFW, LepsFW
 
 from pymatgen import Lattice, IStructure
-from monty.json import MontyDecoder
 
 
 __author__ = 'Anubhav Jain, Kiran Mathew'
@@ -93,18 +92,6 @@ def add_to_lpad(workflow, decorate=False):
 
 
 
-def get_wf_from_spec_dict(structure, wfspec):
-    fws = []
-    for d in wfspec["fireworks"]:
-        modname, classname = d["fw"].rsplit(".", 1)
-        mod = __import__(modname, globals(), locals(), [classname], 0)
-        if hasattr(mod, classname):
-            cls_ = getattr(mod, classname)
-            kwargs = {k: MontyDecoder().process_decoded(v) for k, v in d.get("params", {}).items()}
-            if "parents" in kwargs:
-                kwargs["parents"] = fws[kwargs["parents"]]
-            fws.append(cls_(structure, **kwargs))
-    return Workflow(fws, name=structure.composition.reduced_formula)
 
 
 if __name__ == "__main__":
