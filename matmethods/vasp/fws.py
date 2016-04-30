b07c1a4dcd8529cdbcfa9c7eb359bd23e90fe183
__sp__ <ongsp@eng.ucsd.edu>
2016-04-29 18:42:27 -0700

    Refactoring of get_wfs.

---
+++ matmethods/vasp/fws.py
@@ -0,0 +1,71 @@
+from fireworks import Firework
+
+from matmethods.vasp.firetasks.glue_tasks import PassCalcLocs, CopyVaspOutputs
+from matmethods.vasp.firetasks.parse_outputs import VaspToDbTask
+from matmethods.vasp.firetasks.run_calc import RunVaspDirect
+from matmethods.vasp.firetasks.write_inputs import WriteVaspFromIOSet, \
+    WriteVaspStaticFromPrev, WriteVaspNSCFFromPrev, WriteVaspDFPTDielectricFromPrev
+from matmethods.vasp.input_sets import StructureOptimizationVaspInputSet
+from matmethods.vasp.vasp_powerups import decorate_write_name
+from matmethods.vasp.workflows.base.single_vasp import get_wf_single
+
+
+class OptimizeFW(Firework):
+
+    def __init__(self, structure, name="structure optimization", vasp_input_set=None, vasp_cmd="vasp",
+                 db_file=None, parents=None, **kwargs):
+        t1 = []
+        t1.append(WriteVaspFromIOSet(structure=structure, vasp_input_set=vasp_input_set))
+        t1.append(RunVaspDirect(vasp_cmd=vasp_cmd))
+        t1.append(PassCalcLocs(name=name))
+        t1.append(VaspToDbTask(db_file=db_file,
+                               additional_fields={"task_label": name}))
+        super(OptimizeFW, self).__init__(t1, parents=parents, name="{}-{}".format(
+            structure.composition.reduced_formula, name), **kwargs)
+
+
+class StaticFW(Firework):
+
+    def __init__(self, name="static", vasp_cmd="vasp", copy_vasp_outputs=True,
+                 db_file=None, parents=None, **kwargs):
+
+        t2 = []
+        if copy_vasp_outputs:
+            t2.append(CopyVaspOutputs(calc_loc=True))
+        t2.append(WriteVaspStaticFromPrev())
+        t2.append(RunVaspDirect(vasp_cmd=vasp_cmd))
+        t2.append(PassCalcLocs(name=name))
+        t2.append(VaspToDbTask(db_file=db_file, additional_fields={"task_label": name}))
+        super(StaticFW, self).__init__(t2, parents=parents, name=name, **kwargs)
+
+
+class NonSCFUniformFW(Firework):
+
+    def __init__(self, name="nscf uniform", vasp_cmd="vasp", copy_vasp_outputs=True,
+                 db_file=None, parents=None, **kwargs):
+        t3 = []
+        if copy_vasp_outputs:
+            t3.append(CopyVaspOutputs(calc_loc=True, additional_files=["CHGCAR"]))
+        t3.append(WriteVaspNSCFFromPrev(mode="uniform"))
+        t3.append(RunVaspDirect(vasp_cmd=vasp_cmd))
+        t3.append(PassCalcLocs(name=name))
+        t3.append(VaspToDbTask(db_file=db_file,
+                               additional_fields={"task_label": name},
+                               parse_dos=True, bandstructure_mode="uniform"))
+        super(NonSCFUniformFW, self).__init__(t3, parents=parents, name=name, **kwargs)
+
+
+class NonSCFLineFW(Firework):
+
+    def __init__(self, name="nscf line", vasp_cmd="vasp", copy_vasp_outputs=True,
+                 db_file=None, parents=None, **kwargs):
+        t4 = []
+        if copy_vasp_outputs:
+            t4.append(CopyVaspOutputs(calc_loc=True, additional_files=["CHGCAR"]))
+        t4.append(WriteVaspNSCFFromPrev(mode="line"))
+        t4.append(RunVaspDirect(vasp_cmd=vasp_cmd))
+        t4.append(PassCalcLocs(name=name))
+        t4.append(VaspToDbTask(db_file=db_file,
+                               additional_fields={"task_label": name},
+                               bandstructure_mode="line"))
+        super(NonSCFLineFW, self).__init__(t4, parents=parents, name=name, **kwargs)
--- matmethods/vasp/workflows/base/band_structure.py
+++ matmethods/vasp/workflows/base/band_structure.py
@@ -17,6 +17,8 @@ from matmethods.vasp.input_sets import StructureOptimizationVaspInputSet
 from matmethods.vasp.vasp_powerups import decorate_write_name
 from matmethods.vasp.workflows.base.single_vasp import get_wf_single
 
+from matmethods.vasp.fws import OptimizeFW, StaticFW, NonSCFUniformFW, NonSCFLineFW
+
 from pymatgen import Lattice, IStructure
 
 
@@ -66,48 +68,14 @@ def get_wf_bandstructure(structure, vasp_input_set=None, vasp_cmd="vasp",
      """
     vasp_input_set = vasp_input_set if vasp_input_set else StructureOptimizationVaspInputSet()
 
-    task_label = "structure optimization"
-    t1 = []
-    t1.append(WriteVaspFromIOSet(structure=structure, vasp_input_set=vasp_input_set))
-    t1.append(RunVaspDirect(vasp_cmd=vasp_cmd))
-    t1.append(PassCalcLocs(name=task_label))
-    t1.append(VaspToDbTask(db_file=db_file,
-                           additional_fields={"task_label": task_label}))
-    fw1 = Firework(t1, name="{}-{}".format(structure.composition.reduced_formula, task_label))
+    common_kwargs = {"vasp_cmd": vasp_cmd, "db_file": db_file}
 
-    task_label = "static"
-    t2 = []
-    t2.append(CopyVaspOutputs(calc_loc=True))
-    t2.append(WriteVaspStaticFromPrev())
-    t2.append(RunVaspDirect(vasp_cmd=vasp_cmd))
-    t2.append(PassCalcLocs(name=task_label))
-    t2.append(VaspToDbTask(db_file=db_file, additional_fields={"task_label": task_label}))
-    fw2 = Firework(t2, parents=fw1, name="{}-{}".format(structure.composition.reduced_formula,
-                                                        task_label))
-
-    task_label = "nscf uniform"
-    t3 = []
-    t3.append(CopyVaspOutputs(calc_loc=True, additional_files=["CHGCAR"]))
-    t3.append(WriteVaspNSCFFromPrev(mode="uniform"))
-    t3.append(RunVaspDirect(vasp_cmd=vasp_cmd))
-    t3.append(PassCalcLocs(name=task_label))
-    t3.append(VaspToDbTask(db_file=db_file,
-                           additional_fields={"task_label": task_label},
-                           parse_dos=True, bandstructure_mode="uniform"))
-    fw3 = Firework(t3, parents=fw2, name="{}-{}".format(structure.composition.reduced_formula,
-                                                        task_label))
+    fw1 = OptimizeFW(structure=structure, vasp_input_set=vasp_input_set, **common_kwargs)
+    fw2 = StaticFW(copy_vasp_outputs=True, parents=fw1, **common_kwargs)
+    fw3 = NonSCFUniformFW(copy_vasp_outputs=True, parents=fw2, **common_kwargs)
+    fw4 = NonSCFLineFW(copy_vasp_outputs=True, parents=fw2, **common_kwargs)
 
     # line mode (run in parallel to uniform)
-    t4 = []
-    task_label = "nscf line"
-    t4.append(CopyVaspOutputs(calc_loc=True, additional_files=["CHGCAR"]))
-    t4.append(WriteVaspNSCFFromPrev(mode="line"))
-    t4.append(RunVaspDirect(vasp_cmd=vasp_cmd))
-    t4.append(PassCalcLocs(name=task_label))
-    t4.append(VaspToDbTask(db_file=db_file, additional_fields={"task_label": task_label},
-                           bandstructure_mode="line"))
-    fw4 = Firework(t4, parents=fw2, name="{}-{}".format(structure.composition.reduced_formula,
-                                                        task_label))
 
     wf = [fw1, fw2, fw3, fw4]
 
@@ -146,5 +114,6 @@ if __name__ == "__main__":
                        [1.9200989668, 3.3257101909, 0.00],
                        [0.00, -2.2171384943, 3.1355090603]])
     structure = IStructure(lattice, ["Si"] * 2, coords)
-    wf = get_wf_single(structure)
-    add_to_lpad(wf, decorate=True)
+    wf = get_wf_bandstructure(structure)
+    #wf = get_wf_single(structure)
+    #add_to_lpad(wf, decorate=True)
