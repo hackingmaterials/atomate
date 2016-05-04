df8a011033e7875d77192287b8511f52b4db4d0c
Anubhav Jain <ajain@lbl.gov>
2016-05-04 13:36:29 -0700

    fix refactor

--- matmethods/vasp/examples/add_vasp_wfs_to_db.py
+++ matmethods/vasp/examples/add_vasp_wfs_to_db.py
@@ -1,5 +1,5 @@
 from fireworks import LaunchPad
-from matmethods.vasp.workflows.automatic.standard import wf_band_structure
+from matmethods.vasp.workflows.automatic.core import wf_band_structure
 from pymatgen import Structure
 
 __author__ = 'Anubhav Jain, Kiran Mathew'
