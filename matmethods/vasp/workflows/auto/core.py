7558d0b2c42499a449867921bc3eb83508c3f99a
Anubhav Jain <ajain@lbl.gov>
2016-05-12 13:43:45 -0700

    add auto method for getting dielectric constant

--- matmethods/vasp/workflows/auto/core.py
+++ matmethods/vasp/workflows/auto/core.py
@@ -1,3 +1,4 @@
+from matmethods.vasp.workflows.base.optical import get_wf_dielectric_constant
 from pymatgen.io.vasp.sets import MPVaspInputSet, MPStaticSet
 
 from matmethods.vasp.vasp_powerups import use_custodian, add_namefile, \
@@ -20,8 +21,9 @@ def wf_band_structure(structure):
     :param structure:
     :return:
     """
-    wf = get_wf_bandstructure(structure, vasp_input_set=MPVaspInputSet(force_gamma=True),
-                              vasp_cmd=">>vasp_cmd<<", db_file=">>db_file<<")
+    wf = get_wf_bandstructure(
+        structure, vasp_input_set=MPVaspInputSet(force_gamma=True),
+        vasp_cmd=">>vasp_cmd<<", db_file=">>db_file<<")
     wf = use_custodian(wf)
     wf = use_custodian(wf, fw_name_constraint="structure optimization",
                        custodian_params={"job_type": "double_relaxation_run",
@@ -40,8 +42,10 @@ def wf_static(structure):
     :param structure:
     :return:
     """
-    wf = get_wf_single(structure, vasp_input_set=MPStaticSet(structure), vasp_cmd=">>vasp_cmd<<",
-                       db_file=">>db_file<<", task_label="static")
+    wf = get_wf_single(
+        structure, vasp_input_set=MPStaticSet(structure), vasp_cmd=">>vasp_cmd<<",
+        db_file=">>db_file<<", task_label="static")
+
     wf = use_custodian(wf)
     wf = add_namefile(wf)
     wf = use_scratch_dir(wf, ">>scratch_dir<<")
@@ -58,6 +62,21 @@ def wf_structure_optimization(structure):
     wf = get_wf_single(structure, vasp_input_set=MPVaspInputSet(force_gamma=True),
                        vasp_cmd=">>vasp_cmd<<", db_file=">>db_file<<",
                        task_label="structure optimization")
+    wf = use_custodian(wf, fw_name_constraint="structure optimization",
+                       custodian_params={"job_type": "double_relaxation_run",
+                                         "max_force_threshold": 0.25})
+    wf = add_namefile(wf)
+    wf = use_scratch_dir(wf, ">>scratch_dir<<")
+
+    return wf
+
+
+def wf_dielectric_constant(structure):
+    wf = get_wf_dielectric_constant(
+        structure, vasp_input_set=MPVaspInputSet(force_gamma=True),
+        vasp_cmd=">>vasp_cmd<<", db_file=">>db_file<<")
+
+    wf = use_custodian(wf)
     wf = use_custodian(wf, fw_name_constraint="structure optimization",
                        custodian_params={"job_type": "double_relaxation_run",
                                          "max_force_threshold": 0.25})
