856c4d5258218a479ae41d0bd3daa1fe72681848
Shyam Dwaraknath <shyamd@lbl.gov>
2016-05-23 16:39:13 -0700

    Added in stress/strain passing to elastic deformation calculations

--- matmethods/vasp/workflows/base/elastic.py
+++ matmethods/vasp/workflows/base/elastic.py
@@ -1,23 +1,46 @@
 # coding: utf-8
 
-from __future__ import division, print_function, unicode_literals, absolute_import
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import os
+
+from fireworks import Workflow
+
+from matmethods.vasp.fireworks.core import OptimizeFW, TransmuterFW
+from pymatgen.io.vasp.sets import MPVaspInputSet
+from pymatgen.transformations.standard_transformations import \
+    DeformStructureTransformation
 
 """
 This module defines the elastic workflow
 """
 
-from fireworks import Workflow
-import os
-from pymatgen.io.vasp.sets import MPVaspInputSet
-from matmethods.vasp.fireworks.core import OptimizeFW, TransmuterFW
-from pymatgen.transformations.standard_transformations import DeformStructureTransformation
 
 __author__ = 'Shyam Dwaraknath, Joseph Montoya'
 __email__ = 'shyamd@lbl.gov, montoyjh@lbl.gov'
 
 
+@explicit_serialize
+class PassStressStrainData(FireTaskBase):
+    """
+    Passes the stress and deformation for an elastic deformation calculation
+
+    Required params:
+        deformation: the deformation gradient used in the elastic analysis.
+
+    """
+
+    required_params = ["deformation"]
+
+    def run_task(self, fw_spec):
+        v = Vasprun('Vasprun.xml')
+        stress = v['ionic_steps'][-1]['stress']
+        return FWAction(mod_spec=[{'_push': {'deformation': self['deformation'], 'stress': stress}}])
+
+
 def get_wf_elastic_constant(structure, max_norm_deformation=0.01, max_shear_deformation=0.08,
-                 num_norm=4, num_shear=4, vasp_input_set=None, vasp_cmd="vasp",db_file=None):
+                            num_norm=4, num_shear=4, vasp_input_set=None, vasp_cmd="vasp", db_file=None):
     """
     Returns a workflow to calculate elastic consants. This workflow is dynamic
 
@@ -69,35 +92,41 @@ def get_wf_elastic_constant(structure, max_norm_deformation=0.01, max_shear_defo
             deformations.append(defo)
 
     for deformation in deformations:
-        fws.append(TransmuterFW(structure=strcuture,transformations=[DeformStructureTransformation],transformation_params=[{"deformation" : deformation.tolist()}],parents=fws[0]))
+        fw = TransmuterFW(structure=strcuture,
+                          transformations=[DeformStructureTransformation],
+                          transformation_params=[
+                              {"deformation": deformation.tolist()}],
+                          parents=fws[0])
 
+        fw['_tasks'].append(PassStressStrainData(deformation=deformation.tolist()))
+        fws.append(fw)
 
     # TODO: Analyze the data -  OR MAYBE THIS SHOULD BE A BUILDER ????
 
 
-def symm_reduce(self, symm_ops, deformation_list, tolerance = 1e-2):
-        """
-        Checks list of deformation gradient tensors for symmetrical
-        equivalents and returns a new list with reduntant ones removed
+def symm_reduce(self, symm_ops, deformation_list, tolerance=1e-2):
+    """
+    Checks list of deformation gradient tensors for symmetrical
+    equivalents and returns a new list with reduntant ones removed
 
-        Args:
-            symm_ops (list of SymmOps): list of SymmOps objects with which
-                to check the list of deformation tensors for duplicates
-            deformation_list (list of Deformations): list of deformation
-                gradient objects to check for duplicates
-            tolerance (float): tolerance for assigning equal defo. gradients
-        """
-        unique_defos = []
-        for defo in deformation_list:
-            in_unique = False
-            for op in symm_ops:
-                if np.any([(np.abs(defo - defo.transform(symm_op)) < tol).all()
-                           for unique_defo in unique_defos]):
-                    in_unique = True
-                    break
-            if not in_unique:
-                unique_defos += [defo]
-        return unique_defos
+    Args:
+        symm_ops (list of SymmOps): list of SymmOps objects with which
+            to check the list of deformation tensors for duplicates
+        deformation_list (list of Deformations): list of deformation
+            gradient objects to check for duplicates
+        tolerance (float): tolerance for assigning equal defo. gradients
+    """
+    unique_defos = []
+    for defo in deformation_list:
+        in_unique = False
+        for op in symm_ops:
+            if np.any([(np.abs(defo - defo.transform(symm_op)) < tol).all()
+                       for unique_defo in unique_defos]):
+                in_unique = True
+                break
+        if not in_unique:
+            unique_defos += [defo]
+    return unique_defos
 
 
 if __name__ == "__main__":
