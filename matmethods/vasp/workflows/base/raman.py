7f88e9c309cd160baf3ac448680a8344c4f74f5c
Kiran Mathew <kmathew@lbl.gov>
2016-08-25 09:25:08 -0700

    bug fix

--- matmethods/vasp/workflows/base/raman.py
+++ matmethods/vasp/workflows/base/raman.py
@@ -41,7 +41,7 @@ def get_wf_raman_spectra(structure, modes=None, step_size=0.005, vasp_cmd="vasp"
     Returns:
         Workflow
     """
-    modes = 3*len(structure) if not modes else modes
+    modes = range(3*len(structure)) if not modes else modes
     vis = MPRelaxSet(structure, force_gamma=True)
     # displacements in + and - direction along the normal mode so that the central difference scheme
     # can be used for the evaluation of Raman tensor (derivative of epsilon wrt displacement)
