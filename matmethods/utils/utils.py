ab85264b1b4d1aad308fa1f9f3e57b310eaa3de2
Kiran Mathew <kmathew@lbl.gov>
2016-09-09 10:38:50 -0700

    add tools directory: move plot_wf and add plot_raman

--- matmethods/utils/utils.py
+++ matmethods/utils/utils.py
@@ -4,8 +4,8 @@ from __future__ import division, print_function, unicode_literals, absolute_impo
 
 import logging
 import sys
-import six
 
+import six
 from pymatgen import Composition
 
 __author__ = 'Anubhav Jain, Kiran Mathew'
@@ -115,68 +115,3 @@ def get_meta_from_structure(structure):
             'is_ordered': structure.is_ordered,
             'is_valid': structure.is_valid()}
     return meta
-
-
-# TODO: move this code as soon as possible.
-# It causes errors in particular Python installations - and even though there is a try-catch, it does not recover!!!
-#   from matplotlib.backends import _macosx
-# RuntimeError: Python is not installed as a framework. The Mac OS X backend will not be able to function correctly if Python is not installed as a framework. See the Python documentation for more information on installing Python as a framework on Mac OS X. Please either reinstall Python as a framework, or try one of the other backends. If you are Working with Matplotlib in a virtual enviroment see 'Working with Matplotlib in Virtual environments' in the Matplotlib FAQ
-
-def plot_wf(wf, depth_factor=1.0, breadth_factor=2.0, labels_on=True, numerical_label=False,
-            text_loc_factor=1.0, save_as=None, style='rD--', markersize=10, markerfacecolor='blue',
-            fontsize=12,):
-    """
-    Generate a visual representation of the workflow. Useful for checking whether the firework
-    connections are in order before launching the workflow.
-
-    Args:
-        wf (Workflow): workflow object.
-        depth_factor (float): adjust this to stretch the plot in y direction.
-        breadth_factor (float): adjust this to stretch the plot in x direction.
-        labels_on (bool): whether to label the nodes or not. The default is to lable the nodes
-            using the firework names.
-        numerical_label (bool): set this to label the nodes using the firework ids.
-        text_loc_factor (float): adjust the label location.
-        save_as (str): save the figure to the given name.
-        style (str): marker style.
-        markersize (int): marker size.
-        markerfacecolor (str): marker face color.
-        fontsize (int): font size for the node label.
-    """
-    try:
-        import matplotlib.pyplot as plt
-    except ImportError:
-        plt = None
-
-    keys = sorted(wf.links.keys(), reverse=True)
-
-    # set (x,y) coordinates for each node in the workflow links
-    points_map = {}
-    points_map.update({keys[0]: (-0.5*breadth_factor, (keys[0]+1)*depth_factor)})
-    for k in keys:
-        if wf.links[k]:
-            for i, j in enumerate(wf.links[k]):
-                if not points_map.get(j, None):
-                    points_map[j] = ((i-len(wf.links[k])/2.0)*breadth_factor, k*depth_factor)
-
-    # connect the dots
-    for k in keys:
-        for i in wf.links[k]:
-            plt.plot([points_map[k][0], points_map[i][0]], [points_map[k][1], points_map[i][1]],
-                     style, markersize=markersize, markerfacecolor=markerfacecolor)
-            if labels_on:
-                label1 = wf.id_fw[k].name
-                label2 = wf.id_fw[i].name
-                if numerical_label:
-                    label1 = str(k)
-                    label2 = str(i)
-                plt.text(points_map[k][0] * text_loc_factor, points_map[k][1] * text_loc_factor,
-                         label1, fontsize=fontsize)
-                plt.text(points_map[i][0] * text_loc_factor, points_map[i][1] * text_loc_factor,
-                         label2, fontsize=fontsize)
-
-    plt.axis('scaled')
-    plt.axis('off')
-
-    if save_as:
-        plt.savefig(save_as)
