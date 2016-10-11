5f4e9ef77d5c85d94f3584e83f87d6ee66d0b674
__sp__ <ongsp@eng.ucsd.edu>
2016-10-10 22:07:25 -0700

    Add elements key to Materials Builder. Required for any form of more sophisticated queries.

--- matmethods/vasp/builders/tasks_materials.py
+++ matmethods/vasp/builders/tasks_materials.py
@@ -156,7 +156,8 @@ class TasksMaterialsBuilder(AbstractBuilder):
         doc["structure"] = taskdoc["output"]["structure"]
         doc["material_id"] = self.mid_str(self._counter.find_one_and_update(
                         {"_id": "materialid"}, {"$inc": {"c": 1}}, return_document=ReturnDocument.AFTER)["c"])
-        for x in ["formula_anonymous", "formula_pretty", "formula_reduced_abc", "nelements", "chemsys"]:
+        for x in ["formula_anonymous", "formula_pretty", "formula_reduced_abc",
+                  "elements", "nelements", "chemsys"]:
             doc[x] = taskdoc[x]
 
         self._materials.insert_one(doc)
