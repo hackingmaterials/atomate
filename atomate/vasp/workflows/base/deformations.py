5ae927bb6d80ef64c29f192844384f30bade2d06
jdagdelen <jdagdelen@lbl.gov>
2017-01-26 10:30:17 -0800

    Adding the option to turn off initial structure optimization in elasticity workflow.

--- atomate/vasp/workflows/base/deformations.py
+++ atomate/vasp/workflows/base/deformations.py
@@ -23,7 +23,7 @@ logger = get_logger(__name__)
 
 def get_wf_deformations(structure, deformations, name="deformation", vasp_input_set=None,
                         lepsilon=False, vasp_cmd="vasp", db_file=None, user_kpoints_settings=None,
-                        pass_stress_strain=False, tag="", relax_deformed=False, optimize_first=True):
+                        pass_stress_strain=False, tag="", relax_deformed=False, optimize_structure=True):
     """
     Returns a structure deformation workflow.
 
@@ -48,12 +48,21 @@ def get_wf_deformations(structure, deformations, name="deformation", vasp_input_
     Returns:
         Workflow
     """
-    # input set for relaxation
-    vis_relax = vasp_input_set or MPRelaxSet(structure, force_gamma=True)
-    if user_kpoints_settings:
-        v = vis_relax.as_dict()
-        v.update({"user_kpoints_settings": user_kpoints_settings})
-        vis_relax = vis_relax.__class__.from_dict(v)
+
+    fws, parents = [], []
+    if optimize_structure:
+        # input set for relaxation
+        vis_relax = vasp_input_set or MPRelaxSet(structure, force_gamma=True)
+        if user_kpoints_settings:
+            v = vis_relax.as_dict()
+            v.update({"user_kpoints_settings": user_kpoints_settings})
+            vis_relax = vis_relax.__class__.from_dict(v)
+
+        # Structure optimization firework
+        fws = [OptimizeFW(structure=structure, vasp_input_set=vis_relax, vasp_cmd=vasp_cmd,
+                          db_file=db_file, name="{} structure optimization".format(tag))]
+        parents = fws[0]
+
 
     uis_static = {"ISIF": 2, "ISTART":1}
     if relax_deformed:
@@ -64,12 +73,6 @@ def get_wf_deformations(structure, deformations, name="deformation", vasp_input_
     vis_static = MPStaticSet(structure, force_gamma=True, lepsilon=lepsilon,
                              user_kpoints_settings=user_kpoints_settings,
                              user_incar_settings=uis_static)
-    fws, parents = [], []
-    if optimize_first:
-        # Structure optimization firework
-        fws = [OptimizeFW(structure=structure, vasp_input_set=vis_relax, vasp_cmd=vasp_cmd,
-                          db_file=db_file, name="{} structure optimization".format(tag))]
-        parents = fws[0]
 
     # Deformation fireworks with the task to extract and pass stress-strain appended to it.
     for n, deformation in enumerate(deformations):
