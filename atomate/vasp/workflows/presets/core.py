8a9d60d63ee464fe0bf9089e9d2385ab0c5b98f0
bocklund <brandonbocklund@gmail.com>
2017-02-08 16:21:40 -0500

    Increase force accuracy for phonon calculations

--- atomate/vasp/workflows/presets/core.py
+++ atomate/vasp/workflows/presets/core.py
@@ -273,6 +273,10 @@ def wf_gibbs_free_energy(structure, c=None):
                                   t_min=t_min, t_max=t_max, t_step=t_step)
 
     wf = add_modify_incar(wf, modify_incar_params={"incar_update": {"ENCUT": 600, "EDIFF": 1e-6}})
+    if qha_type is not "debye_model":
+        # increase accuracy of forces for phonon calculations
+        wf = add_modify_incar(wf, modify_incar_params={"incar_udpate": {"LREAL":False, "ADDGRID": True}})
+        wf = add_modify_incar(wf, modify_incar_params={"incar_udpate": {"NSW":1}}, fw_name_constraint:'deformation')
 
     wf = add_common_powerups(wf, c)
 
