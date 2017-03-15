aba776e5c9a954c44640d4f6148d58710d837f33
Hanmei Tang <hat003@eng.ucsd.edu>
2017-03-15 10:57:14 -0700

    add RunNEBVaspFake class

--- atomate/vasp/firetasks/run_calc.py
+++ atomate/vasp/firetasks/run_calc.py
@@ -11,6 +11,7 @@ import shlex
 import subprocess
 import os
 import six
+import glob
 
 from pymatgen.io.vasp import Incar, Kpoints, Poscar, Potcar
 from pymatgen.io.vasp.sets import get_vasprun_outcar
@@ -20,7 +21,7 @@ from custodian import Custodian
 from custodian.vasp.handlers import VaspErrorHandler, AliasingErrorHandler, MeshSymmetryErrorHandler, \
     UnconvergedErrorHandler, MaxForceErrorHandler, PotimErrorHandler, FrozenJobErrorHandler, \
     NonConvergingErrorHandler, PositiveEnergyErrorHandler, WalltimeHandler, StdErrHandler
-from custodian.vasp.jobs import VaspJob
+from custodian.vasp.jobs import VaspJob, VaspNEBJob
 from custodian.vasp.validators import VasprunXMLValidator, VaspFilesValidator
 
 from fireworks import explicit_serialize, FiretaskBase
@@ -147,6 +148,11 @@ class RunVaspCustodian(FiretaskBase):
         elif job_type == "full_opt_run":
             jobs = VaspJob.full_opt_run(vasp_cmd, auto_npar=auto_npar, ediffg=self.get("ediffg"),
                                         max_steps=5, half_kpts_first_relax=False)
+        elif job_type == "neb":
+            jobs = [VaspNEBJob(vasp_cmd, output_file="neb_vasp.out", stderr_file="neb_std_err.txt",
+                               suffix="", final=False, auto_npar=self.get("auto_npar", False),
+                               half_kpts=True, auto_gamma=True, auto_continue=False,
+                               gamma_vasp_cmd=gamma_vasp_cmd)]
         else:
             raise ValueError("Unsupported job type: {}".format(job_type))
 
@@ -160,6 +166,9 @@ class RunVaspCustodian(FiretaskBase):
             handlers.append(WalltimeHandler(wall_time=self["wall_time"]))
 
         validators = [VasprunXMLValidator(), VaspFilesValidator()]
+        if job_type == "neb":
+            # CINEB vasprun.xml sometimes incomplete, file structure different
+            validators = []  # TODO: modify custodian validator
 
         c = Custodian(handlers, jobs, validators=validators, max_errors=max_errors,
                       scratch_dir=scratch_dir, gzipped_output=gzip_output)
@@ -206,8 +215,8 @@ class RunVaspFake(FiretaskBase):
      Vasp Emulator
 
      Required params:
-         ref_dir (string): Path to reference vasp run directory with input files
-            in the folder named 'inputs' and output files in the folder named 'outputs'.
+         ref_dir (string): Path to reference vasp run directory with input files in the folder
+            named 'inputs' and output files in the folder named 'outputs'.
 
      Optional params:
          params_to_check (list): optional list of incar parameters to check.
@@ -224,30 +233,30 @@ class RunVaspFake(FiretaskBase):
         user_incar = Incar.from_file(os.path.join(os.getcwd(), "INCAR"))
         ref_incar = Incar.from_file(os.path.join(self["ref_dir"], "inputs", "INCAR"))
 
-        # perform some BASIC tests
+        # Carry out some BASIC tests.
 
-        # check INCAR
+        # Check INCAR
         params_to_check = self.get("params_to_check", [])
         defaults = {"ISPIN": 1, "ISMEAR": 1, "SIGMA": 0.2}
         for p in params_to_check:
             if user_incar.get(p, defaults.get(p)) != ref_incar.get(p, defaults.get(p)):
                 raise ValueError("INCAR value of {} is inconsistent!".format(p))
 
-        # check KPOINTS
+        # Check KPOINTS
         user_kpoints = Kpoints.from_file(os.path.join(os.getcwd(), "KPOINTS"))
         ref_kpoints = Kpoints.from_file(os.path.join(self["ref_dir"], "inputs", "KPOINTS"))
         if user_kpoints.style != ref_kpoints.style or user_kpoints.num_kpts != ref_kpoints.num_kpts:
             raise ValueError("KPOINT files are inconsistent! Paths are:\n{}\n{}".format(
                 os.getcwd(), os.path.join(self["ref_dir"], "inputs")))
 
-        # check POSCAR
+        # Check POSCAR
         user_poscar = Poscar.from_file(os.path.join(os.getcwd(), "POSCAR"))
         ref_poscar = Poscar.from_file(os.path.join(self["ref_dir"], "inputs", "POSCAR"))
         if user_poscar.natoms != ref_poscar.natoms or user_poscar.site_symbols != ref_poscar.site_symbols:
             raise ValueError("POSCAR files are inconsistent! Paths are:\n{}\n{}".format(
                 os.getcwd(), os.path.join(self["ref_dir"], "inputs")))
 
-        # check POTCAR
+        # Check POTCAR
         user_potcar = Potcar.from_file(os.path.join(os.getcwd(), "POTCAR"))
         ref_potcar = Potcar.from_file(os.path.join(self["ref_dir"], "inputs", "POTCAR"))
         if user_potcar.symbols != ref_potcar.symbols:
@@ -268,3 +277,117 @@ class RunVaspFake(FiretaskBase):
             if os.path.isfile(full_file_name):
                 shutil.copy(full_file_name, os.getcwd())
         logger.info("RunVaspFake: ran fake VASP, generated outputs")
+
+
+@explicit_serialize
+class RunNEBVaspFake(FiretaskBase):
+    """
+     Vasp Emulator for CI-NEB, which has a different file arrangement. Similar to RunVaspFake class.
+
+     Required params:
+         ref_dir (string): Path to reference vasp run directory with input files in the folder named
+            'inputs' and output files in the folder named 'outputs'.
+
+     Optional params:
+         params_to_check (list): optional list of incar parameters to check.
+     """
+    required_params = ["ref_dir"]
+    optional_params = ["params_to_check"]
+
+    def run_task(self, fw_spec):
+        self._get_params()
+        self._verify_inputs()
+        self._clear_inputs()
+        self._generate_outputs()
+
+    def _get_params(self):
+        """Define some convenient variables."""
+        self.VASP_NEB_OUTPUT_FILES = ['INCAR', 'KPOINTS', 'POTCAR', 'vasprun.xml']
+        self.VASP_NEB_OUTPUT_SUB_FILES = ['CHG', 'CHGCAR', 'CONTCAR', 'DOSCAR', 'EIGENVAL',
+                                          'IBZKPT', 'PCDAT', 'POSCAR', 'PROCAR', 'OSZICAR',
+                                          'OUTCAR', 'REPORT', 'WAVECAR', 'XDATCAR']
+        self.user_dir = os.getcwd()
+        self.ref_dir_input = os.path.join(self["ref_dir"], "inputs")
+        self.ref_dir_output = os.path.join(self["ref_dir"], "outputs")
+
+        user_sdir = glob.glob(os.path.join(os.getcwd(), "[0-9][0-9]"))
+        ref_sdir_input = glob.glob(os.path.join(self["ref_dir"], "inputs", "[0-9][0-9]"))
+        ref_sdir_output = glob.glob(os.path.join(self["ref_dir"], "outputs", "[0-9][0-9]"))
+        user_sdir.sort()
+        ref_sdir_input.sort()
+        ref_sdir_output.sort()
+
+        # Check sub-folders consistence.
+        if len(user_sdir) != len(ref_sdir_input):
+            raise ValueError("Sub-folder numbers are inconsistent! "
+                             "Paths are:\n{}\n{}".format(self.user_dir, self.ref_dir_input))
+        self.user_sdir = user_sdir
+        self.ref_sdir_input = ref_sdir_input
+        self.ref_sdir_output = ref_sdir_output
+
+    def _verify_inputs(self):
+        """Validation of input files under user NEB directory."""
+        user_incar = Incar.from_file(os.path.join(self.user_dir, "INCAR"))
+        ref_incar = Incar.from_file(os.path.join(self.ref_dir_input, "INCAR"))
+
+        # Check INCAR
+        params_to_check = self.get("params_to_check", [])
+        defaults = {"ICHAIN": 0, "LCLIMB": True}
+        for p in params_to_check:
+            if user_incar.get(p, defaults.get(p)) != ref_incar.get(p, defaults.get(p)):
+                raise ValueError("INCAR value of {} is inconsistent!".format(p))
+
+        # Check KPOINTS
+        user_kpoints = Kpoints.from_file(os.path.join(self.user_dir, "KPOINTS"))
+        ref_kpoints = Kpoints.from_file(os.path.join(self.ref_dir_input, "KPOINTS"))
+        if user_kpoints.style != ref_kpoints.style or user_kpoints.num_kpts != ref_kpoints.num_kpts:
+            raise ValueError("KPOINT files are inconsistent! "
+                             "Paths are:\n{}\n{}".format(self.user_dir, self.ref_dir_input))
+
+        # Check POTCAR
+        user_potcar = Potcar.from_file(os.path.join(self.user_dir, "POTCAR"))
+        ref_potcar = Potcar.from_file(os.path.join(self.ref_dir_input, "POTCAR"))
+        if user_potcar.symbols != ref_potcar.symbols:
+            raise ValueError("POTCAR files are inconsistent! "
+                             "Paths are:\n{}\n{}".format(self.user_dir, self.ref_dir_input))
+
+        # Check POSCARs
+        for u, r in zip(self.user_sdir, self.ref_sdir_input):
+            user_poscar = Poscar.from_file(os.path.join(u, "POSCAR"))
+            ref_poscar = Poscar.from_file(os.path.join(r, "POSCAR"))
+            if user_poscar.natoms != ref_poscar.natoms or \
+                            user_poscar.site_symbols != ref_poscar.site_symbols:
+                raise ValueError("POSCAR files are inconsistent! Paths are:\n{}\n{}".format(u, r))
+
+        logger.info("Verified inputs successfully.")
+
+    def _clear_inputs(self):
+        """Remove all input files from user NEB directory."""
+        # Clear neb directory
+        for x in self.VASP_NEB_OUTPUT_FILES:
+            p = os.path.join(os.getcwd(), x)
+            if os.path.exists(p):
+                os.remove(p)
+
+        # Clear neb sub-directory
+        for d in self.user_sdir:
+            for x in self.VASP_NEB_OUTPUT_SUB_FILES:
+                p = os.path.join(d, x)
+                if os.path.exists(p):
+                    os.remove(p)
+
+    def _generate_outputs(self):
+        """Copy valid outputs from a reference folder to user NEB directory."""
+        # Copy NEB files.
+        for file_name in os.listdir(self.ref_dir_output):
+            full_file_name = os.path.join(self.ref_dir_output, file_name)
+            if os.path.isfile(full_file_name):
+                shutil.copy(full_file_name, os.getcwd())
+
+        # Copy NEB sub-files.
+        for u_dir, r_dir in zip(self.user_sdir, self.ref_sdir_output):
+            for file_name in os.listdir(r_dir):
+                full_file_name = os.path.join(r_dir, file_name)
+                if os.path.isfile(full_file_name):
+                    shutil.copy(full_file_name, u_dir)
+        logger.info("Ran fake VASP for NEB, generated outputs.")
