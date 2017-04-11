7ef66fbdab563d4b32615c6cbbd11cd836f15641
Anubhav Jain <ajain@lbl.gov>
2017-04-11 15:08:38 -0700

    remove unused import

--- atomate/common/firetasks/glue_tasks.py
+++ atomate/common/firetasks/glue_tasks.py
@@ -4,8 +4,6 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import os
 
-from six import __init__
-
 from atomate.utils.utils import env_chk
 from fireworks import explicit_serialize, FiretaskBase, FWAction
 
