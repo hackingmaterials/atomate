168baaee5ea889dd8f2dc857ba881d1926f44c6c
Anubhav Jain <ajain@lbl.gov>
2017-04-25 16:43:27 -0700

    remove another import

--- atomate/vasp/builders/__init__.py
+++ atomate/vasp/builders/__init__.py
@@ -2,7 +2,6 @@ __author__ = 'Anubhav Jain <ajain@lbl.gov>'
 
 from .base import *
 from .boltztrap_materials import *
-from .example_build import *
 from .file_materials import *
 from .fix_tasks import *
 from .materials_ehull import *
