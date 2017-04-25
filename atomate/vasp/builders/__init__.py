592842a2850492ec7beed93b1194e702fa015c89
Anubhav Jain <ajain@lbl.gov>
2017-04-25 16:46:11 -0700

    comments

--- atomate/vasp/builders/__init__.py
+++ atomate/vasp/builders/__init__.py
@@ -1,5 +1,9 @@
 __author__ = 'Anubhav Jain <ajain@lbl.gov>'
 
+# TODO: @matk86 - can we please remove all this shortcut baggage? I don't want to have to remember
+# to always add these shortcuts time I add a builder. If you don't like to type vasp.builders
+# try an IDE to auto-complete instead of polluting the code -computron
+
 from .base import *
 from .boltztrap_materials import *
 from .file_materials import *
