b8a46638e95310c808bda36d64b5cecf579bfbdc
Anubhav Jain <ajain@lbl.gov>
2017-05-08 15:24:04 -0700

    more options for OptimizeFW

--- atomate/vasp/fireworks/core.py
+++ atomate/vasp/fireworks/core.py
@@ -25,27 +25,27 @@ from atomate.vasp.firetasks.neb_tasks import WriteNEBFromImages, WriteNEBFromEnd
 
 class OptimizeFW(Firework):
     def __init__(self, structure, name="structure optimization", vasp_input_set=None,
-                 vasp_cmd="vasp",
-                 override_default_vasp_params=None, ediffg=None, db_file=None,
-                 force_gamma=True, parents=None, **kwargs):
+                 vasp_cmd="vasp", override_default_vasp_params=None, ediffg=None, db_file=None,
+                 force_gamma=True, job_type="double_relaxation_run", max_force_threshold=0.25,
+                 auto_npar=">>auto_npar<<", parents=None, **kwargs):
         """
-        Standard structure optimization Firework.
+        Optimize the given structure.
 
         Args:
             structure (Structure): Input structure.
             name (str): Name for the Firework.
-            vasp_input_set (VaspInputSet): input set to use.
-                Defaults to MPRelaxSet() if None.
-            override_default_vasp_params (dict): If this is not None,
-                these params are passed to the default vasp_input_set, i.e.,
-                MPRelaxSet. This allows one to easily override some
-                settings, e.g., user_incar_settings, etc.
+            vasp_input_set (VaspInputSet): input set to use. Defaults to MPRelaxSet() if None.
+            override_default_vasp_params (dict): If this is not None, these params are passed to 
+                the default vasp_input_set, i.e., MPRelaxSet. This allows one to easily override 
+                some settings, e.g., user_incar_settings, etc.
             vasp_cmd (str): Command to run vasp.
             ediffg (float): Shortcut to set ediffg in certain jobs
-            db_file (str): Path to file specifying db credentials.
+            db_file (str): Path to file specifying db credentials to place output parsing.
             force_gamma (bool): Force gamma centered kpoint generation
-            parents (Firework): Parents of this particular Firework.
-                FW or list of FWS.
+            job_type (str): custodian job type (default "double_relaxation_run")
+            max_force_threshold (float): max force on a site allowed at end; otherwise, reject job
+            auto_npar (bool or str): whether to set auto_npar. defaults to env_chk: ">>auto_npar<<"
+            parents ([Firework]): Parents of this particular Firework.
             \*\*kwargs: Other kwargs that are passed to Firework.__init__.
         """
         override_default_vasp_params = override_default_vasp_params or {}
@@ -54,9 +54,9 @@ class OptimizeFW(Firework):
 
         t = []
         t.append(WriteVaspFromIOSet(structure=structure, vasp_input_set=vasp_input_set))
-        t.append(RunVaspCustodian(vasp_cmd=vasp_cmd, job_type="double_relaxation_run",
-                                  max_force_threshold=0.25, ediffg=ediffg,
-                                  auto_npar=">>auto_npar<<"))
+        t.append(RunVaspCustodian(vasp_cmd=vasp_cmd, job_type=job_type,
+                                  max_force_threshold=max_force_threshold, ediffg=ediffg,
+                                  auto_npar=auto_npar))
         t.append(PassCalcLocs(name=name))
         t.append(VaspToDbTask(db_file=db_file, additional_fields={"task_label": name}))
         super(OptimizeFW, self).__init__(t, parents=parents, name="{}-{}".
@@ -504,7 +504,7 @@ class NEBFW(Firework):
                                         gamma_vasp_cmd=">>gamma_vasp_cmd<<",
                                         **cust_args)
 
-        # Task 3, 4: Transfer and PassCalLocs
+        # Task 3, 4: Transfer and PassCalcLocs
         tasks = [write_neb_task, run_neb_task, TransferNEBTask(label=label),
                  PassCalcLocs(name=label)]
 
