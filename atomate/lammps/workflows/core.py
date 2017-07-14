5a80a75c6ebc535ea528e046a662e1779d3ac84e
Kiran Mathew <kmathew@lbl.gov>
2017-07-14 16:06:19 -0700

    cleanup docstrings

--- atomate/lammps/workflows/core.py
+++ atomate/lammps/workflows/core.py
@@ -43,23 +43,25 @@ def get_wf_from_input_template(input_template_file, lammps_data, input_filename=
                                is_forcefield=False, lammps_cmd="lammps", db_file=None,
                                name="LAMMPS template Wflow"):
     """
-    Returns workflow where the input file parameters are set from the give json template file.
+    Returns workflow where the input file parameters are set from the give json(or plain text)
+    template file.
 
     Args:
-        input_template_file: json template input file
+        input_template_file (str): path to json or plain text template lammps input file.
         lammps_data (string/LammpsData/LammpsForceFieldData): path to the data file or
-            an appropriate object
-        input_filename (string): input file name
-        data_filename (string): name of the the lammps data file
+            an appropriate object.
+        input_filename (string): input file name. This is the name of the input file passed to the
+            lammps binary.
+        data_filename (string): name of the the lammps data file.
         user_lammps_settings (dict): User lammps settings
         is_forcefield (bool): whether the data file has forcefield and topology info in it.
             This is required only if lammps_data is a path to the data file instead of a data object.
-        lammps_cmd (string): path to the lammps binary
+        lammps_cmd (string): lammps command to run.
         db_file (string): path to the db file.
+        name (str): workflow name
 
     Returns:
         Workflow
-
     """
     user_lammps_settings = user_lammps_settings or {}
     wf_name = name
@@ -88,11 +90,14 @@ def nvt_wf(lammps_data, input_filename="lammps_nvt.in", data_filename="lammps.da
         lammps_cmd (string): path to the lammps binary
         db_file (string): path to the db file
         name (str): workflow name
+
+    Returns:
+        Workflow
     """
     user_lammps_settings = user_lammps_settings or None
     wf_name = name
     lammps_dict_input = NVTLammpsInput(lammps_data=lammps_data, data_filename=data_filename,
                                        user_lammps_settings=user_lammps_settings,
                                        is_forcefield=is_forcefield)
-    return get_wf(wf_name, lammps_dict_input, input_filename=input_filename, lammps_cmd=lammps_cmd,
-                  db_file=db_file)
+    return get_wf(wf_name, lammps_dict_input, input_filename=input_filename,
+                  data_filename=data_filename, lammps_cmd=lammps_cmd, db_file=db_file)
