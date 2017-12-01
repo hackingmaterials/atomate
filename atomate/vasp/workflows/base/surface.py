2b99558ad3f8c021a11d60e3745a3fa2df61057a
richardtran415 <rit634989@gmail.com>
2017-11-30 18:44:37 -0800

    added tests for surface workflow generation and added simple method to run workflow based on a conventional unit cell only

--- atomate/vasp/workflows/base/surface.py
+++ atomate/vasp/workflows/base/surface.py
@@ -1,15 +1,10 @@
 ## for Surface Energy Calculation
 from __future__ import division, unicode_literals
 
-"""
-#TODO: Write module doc.
-       Clean up
-"""
-
 __author__ = "Richard Tran"
-__version__ = "0.1"
-__email__ = "rit634989@gmail.com"
-__date__ = "6/24/15"
+__version__ = "0.2"
+__email__ = "rit001@eng.ucsd.edu"
+__date__ = "11/30/17"
 
 try:
     # New Py>=3.5 import
@@ -51,9 +46,9 @@ from pymongo import MongoClient
 
 
 class SurfaceDBQueryEngine(QueryEngine):
-    def __init__(self, apikey, db_file,
-                 tasks_coll="surface_tasks",
+    def __init__(self, apikey, db_file, tasks_coll="surface_tasks",
                  prop_coll="surface_properties"):
+
         surf_db_credentials = {'host': dbconfig['host'],
                                'port': dbconfig['port'],
                                'user': dbconfig['user'],
@@ -75,7 +70,7 @@ class SurfaceDBQueryEngine(QueryEngine):
 
         super(SurfaceDBQueryEngine, self).__init__(**surf_db_credentials)
 
-    def get_conventional_ucell(formula_id, symprec=1e-3,
+    def get_conventional_ucell(self, formula_id, symprec=1e-3,
                                angle_tolerance=5):
         """
         Gets the conventional unit cell by querying
@@ -95,17 +90,19 @@ class SurfaceDBQueryEngine(QueryEngine):
         return spa.get_conventional_standard_structure()
 
 
-def get_wflow_from_mpid(mpid, qe, vasp_cmd, scratch_dir, db_file,
-                        launchpad_dir, max_errors=10,
-                        handler_group=[], k_product=50):
-    cwd = os.getcwd()
-    # Check for conventional unit cell optimization
-    conv_ucell_entry = qe.get_entries({"material_id": mpid,
-                                       "structure_type": "conventional_unit_cell"},
-                                      inc_structure="Final")[0]
-    el = conv_ucell_entry.structure[0].species_string
+def get_fw_from_ucell(ucell, vasp_cmd, scratch_dir, db_file,
+                      max_errors=10, handler_group=[], k_product=50,
+                      mpid="--", inc_conv_ucell=False):
 
-    fws = []
+    # Get the pretty formula for naming
+    comp = ucell.composition.reduced_composition.as_dict()
+    pretty_formula = ""
+    for el, stoich in comp.items():
+        pretty_formula += str(el)
+        if stoich != 1:
+            pretty_formula += str(int(stoich))
+
+    # Set up Firetask parameters
     OptimizeFW_kwargs = {"vasp_cmd": vasp_cmd,
                          "handler_group": handler_group,
                          "scratch_dir": scratch_dir,
@@ -113,14 +110,17 @@ def get_wflow_from_mpid(mpid, qe, vasp_cmd, scratch_dir, db_file,
                          "half_kpts_first_relax": True,
                          "max_errors": max_errors}
     VaspToDb_kwargs = {"db_file": db_file}
-    additional_fields = {"material_id": mpid,
+    additional_fields = {"pretty_formula": pretty_formula,
+                         "material_id": mpid,
                          "author": os.environ.get("USER")}
 
-    if not conv_ucell_entry:
-        ucell = qe.get_conventional_ucell(mpid)
+    cwd = os.getcwd()
+    fws = []
+    if inc_conv_ucell:
         mvl = MVLSlabSet(ucell, k_product=k_product, bulk=True)
-        name = "%s_%s_conventional_k%s" % (el, mpid, k_product)
+        name = "%s_%s_conventional_k%s" % (pretty_formula, mpid, k_product)
 
+        # Edit Firetask parameters for particular Firework
         OptimizeFW_kwargs["structure"] = ucell
         OptimizeFW_kwargs["vasp_input_set"] = mvl
         OptimizeFW_kwargs["name"] = name
@@ -128,12 +128,32 @@ def get_wflow_from_mpid(mpid, qe, vasp_cmd, scratch_dir, db_file,
         additional_fields["calculation_nme"] = name
         VaspToDb_kwargs["calc_dir"] = os.path.join(cwd, name)
         VaspToDb_kwargs["additional_fields"] = additional_fields
+
         tasks = [OptimizeFW(**OptimizeFW_kwargs),
                  VaspToDb(**VaspToDb_kwargs)]
+        fws.append(Firework(tasks, name="%s_%s" %(pretty_formula, mpid)))
 
-    fws.append(tasks)
-    wf = Workflow(fws, name='Surface Calculations')
-    launchpad = LaunchPad.from_file(os.path.join(os.environ["HOME"],
-                                                 launchpad_dir,
-                                                 "my_launchpad.yaml"))
-    launchpad.add_wf(wf)
+    return fws
+
+def get_wflow_from_mpid(mpid, qe, vasp_cmd, scratch_dir, db_file,
+                        max_errors=10, handler_group=[], k_product=50):
+
+    # Check for conventional unit cell optimization
+    conv_ucell_entry = qe.get_entries({"material_id": mpid,
+                                       "structure_type": "conventional_unit_cell"},
+                                      inc_structure="Final")
+
+    inc_conv_ucell = True if not conv_ucell_entry else False
+    ucell = conv_ucell_entry.structure if conv_ucell_entry \
+        else qe.get_conventional_ucell(mpid)
+
+    fws = get_tasks_from_ucell(ucell, qe, vasp_cmd, scratch_dir,
+                               db_file, max_errors=max_errors,
+                               handler_group=handler_group, k_product=k_product,
+                               mpid=mpid, inc_conv_ucell=inc_conv_ucell)
+
+    return Workflow(fws, name='Surface Calculations')
+        # launchpad = LaunchPad.from_file(os.path.join(os.environ["HOME"],
+        #                                              launchpad_dir,
+        #                                              "my_launchpad.yaml"))
+        # launchpad.add_wf(wf)
---
+++ atomate/vasp/workflows/tests/test_surface_workflow.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+
+from __future__ import division, print_function, unicode_literals, absolute_import
+
+import os
+import unittest
+
+from fireworks import FWorker
+from fireworks.core.rocket_launcher import rapidfire
+
+from atomate.vasp.powerups import use_fake_vasp
+from atomate.vasp.workflows.base.surface import get_wflow_from_mpid, get_fw_from_ucell
+from atomate.utils.testing import AtomateTest
+
+from pymatgen import Structure, Molecule, Lattice
+from pymatgen.core.surface import generate_all_slabs
+
+__author__ = "Richard Tran"
+__email__ = "rit001@eng.ucsd.edu"
+
+module_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+db_dir = os.path.join(module_dir, "..", "..", "..", "common", "test_files")
+ref_dir = os.path.join(module_dir, "..", "..", "test_files")
+
+DEBUG_MODE = False  # If true, retains the database and output dirs at the end of the test
+VASP_CMD = None  # If None, runs a "fake" VASP. Otherwise, runs VASP with this command...
+
+class TestSurfaceWorkflow(AtomateTest):
+
+    def setUp(self):
+        super(TestSurfaceWorkflow, self).setUp()
+
+        self.struct_ir = Structure.from_spacegroup("Fm-3m", Lattice.cubic(3.875728),
+                                                   ["Ir"], [[0, 0, 0]])
+        self.wf = get_fw_from_ucell(self.struct_ir, "vasp", ".",
+                                    os.path.join(db_dir, db.json),
+                                    inc_conv_ucell=True)
+        self.name = "%s_%s_conventional_k%s" %("Ni", "--", 50)
+        self.cwd = os.getcwd()
+
+    def run_wf(self):
+        ref_dir = os.path.join(self.cwd, self.name)
+        use_fake_vasp(original_wf, {"Ni_--": ref_dir})
+
+    # def cleanup(self):
+
+if __name__ == "__main__":
+    unittest.main()
