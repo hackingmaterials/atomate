f9e189445bf77ebab2e0812c71328ed9efa0742c
shyamd <shyamd@lbl.gov>
2017-12-22 09:27:27 -0800

    update wavecar removal logic

--- atomate/vasp/workflows/presets/core.py
+++ atomate/vasp/workflows/presets/core.py
@@ -11,7 +11,7 @@ from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 from pymatgen.io.vasp.inputs import Kpoints
 
 from atomate.vasp.config import SMALLGAP_KPOINT_MULTIPLY, STABILITY_CHECK, VASP_CMD, DB_FILE, \
-    ADD_WF_METADATA, HALF_KPOINTS_FIRST_RELAX
+    ADD_WF_METADATA, HALF_KPOINTS_FIRST_RELAX, REMOVE_WAVECAR
 from atomate.vasp.powerups import add_small_gap_multiply, add_stability_check, add_modify_incar, \
     add_wf_metadata, add_common_powerups, use_custodian, clean_up_files
 from atomate.vasp.workflows.base.core import get_wf
@@ -77,7 +77,7 @@ def wf_scan_opt(structure,c={}):
     if c.get("ADD_WF_METADATA", ADD_WF_METADATA):
         wf = add_wf_metadata(wf, structure)
 
-    if not c.get("KEEP_WAVECAR", False):
+    if c.get("REMOVE_WAVECAR", REMOVE_WAVECAR):
         wf = clean_up_files(wf)
 
     return wf
