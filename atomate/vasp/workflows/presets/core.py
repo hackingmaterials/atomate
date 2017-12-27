09cc64013c3531db2cb9da18a69ca024e21c81df
shyamd <shyamd@lbl.gov>
2017-12-27 08:23:21 -0800

    Move wf_scan_opt to separate module

--- atomate/vasp/workflows/presets/core.py
+++ atomate/vasp/workflows/presets/core.py
@@ -53,35 +53,6 @@ def wf_bandstructure(structure, c=None):
 
     return wf
 
-def wf_scan_opt(structure,c={}):
-
-    c = c or {}
-    vasp_cmd = c.get("VASP_CMD", VASP_CMD)
-    db_file = c.get("DB_FILE", DB_FILE)
-    user_incar_settings = c.get("USER_INCAR_SETTINGS")
-    half_kpts = c.get("HALF_KPOINTS_FIRST_RELAX",HALF_KPOINTS_FIRST_RELAX)
-    ediffg = user_incar_settings.get("EDIFFG",-0.05) if user_incar_settings else -0.05
-
-    wf = get_wf(structure, "optimize_only.yaml",
-                vis=MVLScanRelaxSet(structure, user_incar_settings=user_incar_settings),
-                common_params={"vasp_cmd": vasp_cmd, "db_file": db_file})
-
-    wf = use_custodian(wf,custodian_params={"ediffg":ediffg,
-                                            "max_force_threshold":0,
-                                            "half_kpts_first_relax":half_kpts,
-                                            "job_type":"metagga_relaxation_run",
-                                            "db_file":db_file,
-                                            "vasp_cmd":vasp_cmd})
-    wf = add_common_powerups(wf, c)
-
-    if c.get("ADD_WF_METADATA", ADD_WF_METADATA):
-        wf = add_wf_metadata(wf, structure)
-
-    if c.get("REMOVE_WAVECAR", REMOVE_WAVECAR):
-        wf = clean_up_files(wf)
-
-    return wf
-
 def wf_bandstructure_plus_hse(structure, gap_only=True, c=None):
 
     c = c or {}
