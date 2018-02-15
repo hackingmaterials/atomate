89b52c8af2566e071991be4a62f4bcc54a005007
richardtran415 <rit634989@gmail.com>
2018-02-14 22:00:16 -0800

    create folder before generating inputs

--- atomate/vasp/workflows/base/surface.py
+++ atomate/vasp/workflows/base/surface.py
@@ -30,7 +30,6 @@ from custodian.vasp.handlers import VaspErrorHandler, NonConvergingErrorHandler,
     FrozenJobErrorHandler
 
 from atomate.vasp.firetasks.run_calc import RunVaspCustodian
-from atomate.vasp.fireworks.core import OptimizeFW
 from atomate.vasp.firetasks.parse_outputs import VaspToDb
 from atomate.common.firetasks.glue_tasks import CreateFolder
 from atomate.vasp.firetasks.write_inputs import WriteVaspFromIOSet
@@ -66,14 +65,13 @@ class SurfacePropertiesWF(object):
 
     def __init__(self, apikey, db_file, tasks_coll="surface_tasks",
                  prop_coll="surface_properties", production_mode=False,
-                 scratch_dir="", k_product=45, vasp_cmd="vasp", ediffg=-0.02,):
+                 scratch_dir="", k_product=50, vasp_cmd="vasp"):
 
         with open(db_file) as data_file:
             dbconfig = json.load(data_file)
 
         self.k_product = k_product
         self.vasp_cmd = vasp_cmd
-        self.ediffg = ediffg
         self.dbconfig = dbconfig
         self.db_file = db_file
         self.qe = SurfaceDBQueryEngine(dbconfig, apikey)
@@ -93,8 +91,8 @@ class SurfacePropertiesWF(object):
         # RunVaspCustodian, PassCalcLocs and VaspToDB. We can then
         # modify or remove tasks to suit our needs.
         tasks = []
-        tasks.append(WriteVaspFromIOSet(structure=ucell, vasp_input_set=mvl))
         tasks.append(CreateFolder(folder_name=name, change_dir=True))
+        tasks.append(WriteVaspFromIOSet(structure=ucell, vasp_input_set=mvl))
         tasks.append(RunVaspCustodian(vasp_cmd=self.vasp_cmd,
                                       auto_npar=">>auto_npar<<",
                                       job_type="double_relaxation_run",
@@ -114,9 +112,54 @@ class SurfacePropertiesWF(object):
             additional_fields["material_id"] = mpid
 
         tasks.append(VaspToDb(additional_fields=additional_fields, db_file=self.db_file))
-        optimizeFW.tasks = tasks
 
-        return Workflow([optimizeFW])
+        return Workflow([Firework(tasks, name=name)])
+
+    # class BasicSurfaceFW(Firework):
+    #     def __init__(self, structure, tasks_coll, prop_coll, production_mode,
+    #                  name, scratch_dir, k_product, db_file, vasp_cmd, **kwargs):
+    #         """
+    #         Optimize the given structure.
+    #
+    #         Args:
+    #             structure (Structure): Input structure.
+    #             tasks_coll
+    #             name (str): Name for the Firework.
+    #             vasp_input_set (VaspInputSet): input set to use. Defaults to MPRelaxSet() if None.
+    #             override_default_vasp_params (dict): If this is not None, these params are passed to
+    #                 the default vasp_input_set, i.e., MPRelaxSet. This allows one to easily override
+    #                 some settings, e.g., user_incar_settings, etc.
+    #             vasp_cmd (str): Command to run vasp.
+    #             ediffg (float): Shortcut to set ediffg in certain jobs
+    #             db_file (str): Path to file specifying db credentials to place output parsing.
+    #             force_gamma (bool): Force gamma centered kpoint generation
+    #             job_type (str): custodian job type (default "double_relaxation_run")
+    #             max_force_threshold (float): max force on a site allowed at end; otherwise, reject job
+    #             auto_npar (bool or str): whether to set auto_npar. defaults to env_chk: ">>auto_npar<<"
+    #             half_kpts_first_relax (bool): whether to use half the kpoints for the first relaxation
+    #             parents ([Firework]): Parents of this particular Firework.
+    #             \*\*kwargs: Other kwargs that are passed to Firework.__init__.
+    #         """
+    #         override_default_vasp_params = override_default_vasp_params or {}
+    #         vasp_input_set = vasp_input_set or MPRelaxSet(structure,
+    #                                                       force_gamma=force_gamma,
+    #                                                       **override_default_vasp_params)
+    #
+    #         t = []
+    #         t.append(WriteVaspFromIOSet(structure=structure,
+    #                                     vasp_input_set=vasp_input_set))
+    #         t.append(RunVaspCustodian(vasp_cmd=vasp_cmd, job_type=job_type,
+    #                                   max_force_threshold=max_force_threshold,
+    #                                   ediffg=ediffg,
+    #                                   auto_npar=auto_npar,
+    #                                   half_kpts_first_relax=half_kpts_first_relax))
+    #         t.append(PassCalcLocs(name=name))
+    #         t.append(
+    #             VaspToDb(db_file=db_file, additional_fields={"task_label": name}))
+    #         super(OptimizeFW, self).__init__(t, parents=parents, name="{}-{}".
+    #                                          format(
+    #             structure.composition.reduced_formula, name),
+    #                                          **kwargs)
 
 
 # @explicit_serialize
