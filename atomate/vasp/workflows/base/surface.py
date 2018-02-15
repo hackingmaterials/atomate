3b656544ee28afb2aafe87e3f870d4ff36aecf1f
richardtran415 <rit634989@gmail.com>
2018-02-15 13:57:39 -0800

    added FacetFWsGeneratorTask along with repetitive optimization tasks to be combined into one later

--- atomate/vasp/workflows/base/surface.py
+++ atomate/vasp/workflows/base/surface.py
@@ -33,9 +33,10 @@ from atomate.vasp.firetasks.run_calc import RunVaspCustodian
 from atomate.vasp.firetasks.parse_outputs import VaspToDb
 from atomate.common.firetasks.glue_tasks import CreateFolder
 from atomate.vasp.firetasks.write_inputs import WriteVaspFromIOSet
+from atomate.vasp.firetasks.glu_tasks import pass_vasp_result
 
 from pymatgen.core.surface import \
-    get_symmetrically_distinct_miller_indices, generate_all_slabs
+    get_symmetrically_distinct_miller_indices, generate_all_slabs, Structure
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 from pymatgen import MPRester
 from pymatgen.analysis.structure_analyzer import VoronoiConnectivity
@@ -61,7 +62,7 @@ class SurfacePropertiesWF(object):
     (for work function).
     """
 
-    def __init__(self, apikey, db_file, tasks_coll="surface_tasks",
+    def __init__(self, db_file, tasks_coll="surface_tasks",
                  prop_coll="surface_properties", production_mode=False,
                  scratch_dir="", k_product=50, vasp_cmd="vasp"):
 
@@ -72,28 +73,25 @@ class SurfacePropertiesWF(object):
         self.vasp_cmd = vasp_cmd
         self.dbconfig = dbconfig
         self.db_file = db_file
-        self.qe = SurfaceDBQueryEngine(dbconfig, apikey)
         self.scratch_dir = scratch_dir
         self.production_mode = production_mode
         self.tasks_coll = tasks_coll
         self.prop_coll = prop_coll
 
-    def wf_from_mpid(self, mpid):
+    def wf_from_mpid(self, structure, mmi, mpid=None):
 
-        return Workflow([BasicSurfaceFW(self.qe, self.tasks_coll, self.prop_coll,
+        return Workflow([ConvUcellFW(structure, mmi, self.tasks_coll, self.prop_coll,
                                         self.production_mode, self.scratch_dir, self.k_product,
                                         self.db_file, self.vasp_cmd, mpid=mpid)])
 
-class BasicSurfaceFW(Firework):
-    def __init__(self, qe, tasks_coll, prop_coll, production_mode,
-                 scratch_dir, k_product, db_file, vasp_cmd,
-                 mpid="", structure=None, **kwargs):
+class ConvUcellFW(Firework):
+    def __init__(self, ucell, mmi, tasks_coll, prop_coll,
+                 production_mode, scratch_dir, k_product,
+                 db_file, vasp_cmd, mpid="--", **kwargs):
         """
-        Optimize the given structure.
+        Customized FW similar to OptimizeFW.
 
         Args:
-            qe (SurfaceDBQueryEngine): For querying structures from the MPAPI
-                or your own DB
             tasks_coll (str): Name of the collection to store the raw data
             prop_coll (str): Name of the collection to store the post-processed
                 data (to be queried from the MAPI)
@@ -109,14 +107,12 @@ class BasicSurfaceFW(Firework):
             vasp_cmd (str): Command to run vasp.
             structure (Structure): Input structure. Need to input either the
                 structure or an mpid
-            mpid (str): Unique Materials Project structure id. Need to input
-                either the structure or an mpid
+            mpid (str): Unique Materials Project structure ID of the unit cell.
             \*\*kwargs: Other kwargs that are passed to Firework.__init__.
         """
 
-        ucell = qe.mprester.get_entry_by_material_id(mpid, inc_structure=True,
-                                                     conventional_unit_cell=True).structure
-        name = "%s_%s_conventional_unit_cell_k%s" % (ucell[0].species_string, mpid, k_product)
+        name = "%s_%s_conventional_unit_cell_k%s" % \
+               (ucell[0].species_string, mpid, k_product)
         mvl = MVLSlabSet(ucell, k_product=k_product, bulk=True)
 
         # This will give us our four basic tasks: WriteVaspFromIOSet,
@@ -131,20 +127,243 @@ class BasicSurfaceFW(Firework):
                                       scratch_dir=scratch_dir))
 
         # Add additional fields to distinguish this calculation
-        additional_fields = {"author": os.environ.get("USER"),
-                             "structure_type": "conventional_unit_cell",
+        additional_fields = {"structure_type": "conventional_unit_cell",
                              "calculation_name": name}
-        # Add mpid as optional so we won't get None
-        # when looking for mpid of isolated atoms
         additional_fields["conventional_spacegroup"] = \
             SpacegroupAnalyzer(ucell).get_space_group_symbol()
         additional_fields["initial_structure"] = ucell
-        if mpid:
-            additional_fields["material_id"] = mpid
+        additional_fields["material_id"] = mpid
+        tasks.append(VaspToDb(additional_fields=additional_fields, db_file=db_file))
+        tasks.append(FacetFWsGeneratorTask(structure_type="conventional_unit_cell", mmi=mmi))
 
+        super(ConvUcellFW, self).__init__(tasks, name=name, **kwargs)
+
+class OUCFW(Firework):
+    def __init__(self, ouc, tasks_coll, prop_coll, miller_index, scale_factor,
+                 production_mode, scratch_dir, k_product,
+                 db_file, vasp_cmd, mpid="--", **kwargs):
+        """
+        Customized FW similar to OptimizeFW.
+
+        Args:
+            tasks_coll (str): Name of the collection to store the raw data
+            prop_coll (str): Name of the collection to store the post-processed
+                data (to be queried from the MAPI)
+            production_mode (bool): Whether or not to run addition fws, tasks,
+                operations meant for high-throughput production, set to False for
+                most users who just run a workflow for non high-throughput purposes.
+            scratch_dir: (str) - if specified, uses this directory as the root
+                scratch dir. Supports env_chk.
+            k_product (int): Default to 50, kpoint number * length for a & b
+                directions, also for c direction in bulk calculations.
+            db_file (str): FULL path to file containing the database credentials.
+                Supports env_chk.
+            vasp_cmd (str): Command to run vasp.
+            structure (Structure): Input structure. Need to input either the
+                structure or an mpid
+            mpid (str): Unique Materials Project structure ID of the unit cell.
+            \*\*kwargs: Other kwargs that are passed to Firework.__init__.
+        """
+
+        name = "%s_%s_bulk_k%s_%s%s%s" % \
+               (ouc[0].species_string, mpid, k_product,
+                miller_index[0], miller_index[1], miller_index[2])
+        mvl = MVLSlabSet(ouc, k_product=k_product, bulk=True)
+
+        # This will give us our four basic tasks: WriteVaspFromIOSet,
+        # RunVaspCustodian, PassCalcLocs and VaspToDB. We can then
+        # modify or remove tasks to suit our needs.
+        tasks = []
+        tasks.append(CreateFolder(folder_name="../"+name, change_dir=True))
+        tasks.append(WriteVaspFromIOSet(structure=ouc, vasp_input_set=mvl))
+        tasks.append(RunVaspCustodian(vasp_cmd=vasp_cmd,
+                                      auto_npar=">>auto_npar<<",
+                                      job_type="double_relaxation_run",
+                                      scratch_dir=scratch_dir))
+
+        # Add additional fields to distinguish this calculation
+        additional_fields = {"structure_type": "oriented_unit_cell",
+                             "calculation_name": name}
+        additional_fields["conventional_spacegroup"] = \
+            SpacegroupAnalyzer(ucell).get_space_group_symbol()
+        additional_fields["initial_structure"] = ouc
+        additional_fields["material_id"] = mpid
+        additional_fields["miller_index"] = miller_index
+        additional_fields["scale_factor"] = scale_factor
+        tasks.append(VaspToDb(additional_fields=additional_fields, db_file=db_file))
+        tasks.append(FacetFWsGeneratorTask(structure_type="oriented_unit_cell"))
+
+        super(OUCFW, self).__init__(tasks, name=name, **kwargs)
+
+class SlabFW(Firework):
+    def __init__(self, slab, tasks_coll, prop_coll, slab_gen_params,
+                 miller_index, scale_factor, ouc, shift, ssize, vsize,
+                 production_mode, scratch_dir, k_product, db_file,
+                 vasp_cmd, reconstruction=None, mpid="--", **kwargs):
+        """
+        Customized FW similar to OptimizeFW.
+
+        Args:
+            tasks_coll (str): Name of the collection to store the raw data
+            prop_coll (str): Name of the collection to store the post-processed
+                data (to be queried from the MAPI)
+            production_mode (bool): Whether or not to run addition fws, tasks,
+                operations meant for high-throughput production, set to False for
+                most users who just run a workflow for non high-throughput purposes.
+            scratch_dir: (str) - if specified, uses this directory as the root
+                scratch dir. Supports env_chk.
+            k_product (int): Default to 50, kpoint number * length for a & b
+                directions, also for c direction in bulk calculations.
+            db_file (str): FULL path to file containing the database credentials.
+                Supports env_chk.
+            vasp_cmd (str): Command to run vasp.
+            structure (Structure): Input structure. Need to input either the
+                structure or an mpid
+            mpid (str): Unique Materials Project structure ID of the unit cell.
+            \*\*kwargs: Other kwargs that are passed to Firework.__init__.
+        """
+
+        if not reconstruction:
+            name = "%s_%s_slab_k%s_s%sv%s_%s%s%s_shift%s" % \
+                   (slab[0].species_string, mpid, k_product, ssize, vsize,
+                    miller_index[0], miller_index[1], miller_index[2], shift)
+        else:
+            name = "%s_%s_slab_k%s_s%sv%s_%s" % (slab[0].species_string, mpid, \
+                                                 k_product, ssize, vsize, reconstruction)
+
+        mvl = MVLSlabSet(slab, k_product=k_product, bulk=False, get_wf=True)
+
+        # This will give us our four basic tasks: WriteVaspFromIOSet,
+        # RunVaspCustodian, PassCalcLocs and VaspToDB. We can then
+        # modify or remove tasks to suit our needs.
+        tasks = []
+        tasks.append(CreateFolder(folder_name="../"+name, change_dir=True))
+        tasks.append(WriteVaspFromIOSet(structure=slab, vasp_input_set=mvl))
+        tasks.append(RunVaspCustodian(vasp_cmd=vasp_cmd,
+                                      auto_npar=">>auto_npar<<",
+                                      job_type="double_relaxation_run",
+                                      scratch_dir=scratch_dir))
+
+        # Add additional fields to distinguish this calculation
+        additional_fields = {"structure_type": "slab_cell",
+                             "calculation_name": name}
+        additional_fields["conventional_spacegroup"] = \
+            SpacegroupAnalyzer(ouc).get_space_group_symbol()
+        additional_fields["initial_structure"] = slab
+        additional_fields["material_id"] = mpid
+        additional_fields["miller_index"] = miller_index
+        additional_fields["scale_factor"] = scale_factor
+        additional_fields["oriented_unit_cell"] = ouc
+        additional_fields["slab_size"] = ssize
+        additional_fields["vac_size"] = vsize
+        additional_fields["shift"] = shift
+        # additional_fields["local_potential_along_c"]
+        # additional_fields["efermi"]
         tasks.append(VaspToDb(additional_fields=additional_fields, db_file=db_file))
 
-        super(BasicSurfaceFW, self).__init__(tasks, name=name, **kwargs)
+        super(SlabFW, self).__init__(tasks, name=name, **kwargs)
+
+
+@explicit_serialize
+class FacetFWsGeneratorTask(FiretaskBase):
+    """
+    Task for generating FWs for oriented unit cell calculations or slab
+    calculations. If the initial structure is an oriented unit cell,
+    generate slab fws, if its a conventional unit cell, generate oriented
+    unit cell and reconstruction calculations.
+    """
+
+    required_params = ['structure_type', "mmi", "scratch_dir", "k_product", "db_file", "tasks_coll", "prop_coll"]
+    optional_params = ["slab_gen_params", "production_mode", "mpid"]
+
+    def run_task(self, fw_spec):
+
+        FWs = []
+        if self.get("structure_type") == "conventional_unit_cell":
+            # Then we create a set of FWs for oriented_unit_cell
+            # calculations and reconstructed slabs
+            miller_list = []
+            if self.get("slab_gen_params", None):
+                slab_gen_params = self.get("slab_gen_params", None)
+            else:
+                ucell = Structure.from_file("CONTCAR.relax2.gz")
+                slab_gen_params = {"structure": ucell, "min_slab_size": 10,
+                                   "min_vacuum_size": 10, "center_slab": True,
+                                   "max_normal_search": self.get("mmi"),
+                                   "symmetrize": True,
+                                   "include_reconstructions": True}
+
+            all_slabs = generate_all_slabs(max_index=self.get("mmi"),
+                                           **slab_gen_params)
+            for slab in all_slabs:
+                if slab.reconstruction:
+                    # build a slab fw
+                    FWs.append(SlabFW(slab, self.get("tasks_coll"),
+                                      self.get("prop_coll"), slab_gen_params,
+                                      slab.miller_index, slab.scale_factor,
+                                      slab.oriented_unit_cell, slab.shift,
+                                      slab_gen_params["min_slab_size"],
+                                      slab_gen_params["min_vacuum_size"],
+                                      self.get("production_mode", True),
+                                      self.get("scratch_dir"), self.get("k_product"),
+                                      self.get("db_file"), self.get("vasp_cmd"),
+                                      reconstruction=slab.reconstruction,
+                                      mpid=self.get("mpid", "--")))
+                else:
+                    if tuple(slab.miller_index) in miller_list:
+                        continue
+                    else:
+                        # build a oriented unit cell fw
+                        FWs.append(OUCFW(slab.oriented_unit_cell, self.get("tasks_coll"),
+                                         self.get("prop_coll"), slab.miller_index,
+                                         slab.scale_factor, self.get("production_mode", True),
+                                         self.get("scratch_dir"), self.get("k_product"),
+                                         self.get("db_file"), self.get("vasp_cmd"),
+                                         mpid=self.get("mpid", "--")))
+
+        return FWAction(additions=FWs)
+
+# @explicit_serialize
+# class SurfPropToDbTask(FiretaskBase):
+#     """
+#     Analyzes the stress/strain data of an elastic workflow to produce
+#     an elastic tensor and various other quantities.
+#
+#     Required params:
+#         structure (Structure): structure to use for symmetrization,
+#             input structure.  If an optimization was used, will
+#             look for relaxed structure in calc locs
+#
+#     Optional params:
+#         db_file (str): path to file containing the database credentials.
+#             Supports env_chk. Default: write data to JSON file.
+#         order (int): order of fit to perform
+#         fw_spec_field (str): if set, will update the task doc with the contents
+#             of this key in the fw_spec.
+#         fitting_method (str): if set, will use one of the specified
+#             fitting methods from pymatgen.  Supported methods are
+#             "independent", "pseudoinverse", and "finite_difference."
+#             Note that order 3 and higher required finite difference
+#             fitting, and will override.
+#     """
+#
+#     required_params = ['structure']
+#     optional_params = ['db_file', 'order', 'fw_spec_field', 'fitting_method']
+#
+#     def run_task(self, fw_spec):
+#
+#         # Add additional fields to distinguish this calculation
+#         additional_fields = {"structure_type": "conventional_unit_cell",
+#                              "calculation_name": name}
+#         additional_fields["conventional_spacegroup"] = \
+#             SpacegroupAnalyzer(ucell).get_space_group_symbol()
+#         additional_fields["initial_structure"] = ucell
+#         # Add mpid as optional so we won't get None
+#         # when looking for mpid of isolated atoms
+#         if mpid:
+#             additional_fields["material_id"] = mpid
+#
+#         return FWAction()
 
 
 # @explicit_serialize
@@ -235,8 +454,6 @@ class BasicSurfaceFW(Firework):
 #         drone.assimilate(calc_locs)
 
 
-class SurfPropToDbTask(FiretaskBase):
-    pass
 
 class SurfaceDBQueryEngine(QueryEngine):
     def __init__(self, dbconfig, apikey, tasks_coll="surface_tasks",
