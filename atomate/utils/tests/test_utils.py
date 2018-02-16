0bfef65a971515eff112202db7f9890396ae0bd5
shyamd <shyamd@lbl.gov>
2018-02-15 16:58:19 -0800

    update to use AtomateTest

--- atomate/utils/tests/test_utils.py
+++ atomate/utils/tests/test_utils.py
@@ -13,9 +13,11 @@ from fireworks import FiretaskBase, Firework, Workflow, explicit_serialize, FWAc
 
 from atomate.utils.utils import env_chk, get_logger, get_mongolike, recursive_get_result, recursive_update, get_database, get_uri
 
+from atomate.utils.testing import AtomateTest
+
 __author__ = 'Anubhav Jain <ajain@lbl.gov>'
 
-module_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+MODULE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 
 logger = get_logger(__name__)
 
@@ -34,12 +36,15 @@ class Task2(FiretaskBase):
         return FWAction(stored_data={"color": "yellow"})
 
 
-class UtilsTests(unittest.TestCase):
-    @classmethod
-    def setUp(cls):
-        cls.fw1 = Firework(Task1())
-        cls.fw2 = Firework([Task2(), Task2()], parents=cls.fw1)
-        cls.fw3 = Firework(Task1(), parents=cls.fw1)
+class UtilsTests(AtomateTest):
+
+    def setUp(self):
+
+        super(UtilsTests, self).setUp()
+        self.fw1 = Firework(Task1())
+        self.fw2 = Firework([Task2(), Task2()], parents=self.fw1)
+        self.fw3 = Firework(Task1(), parents=self.fw1)
+
 
     def test_env_chk(self):
         fw_spec_valid = {"_fw_env": {"hello": "there"}}
@@ -100,14 +105,14 @@ class UtilsTests(unittest.TestCase):
         self.assertEqual(d["a"]["b"], [7])
 
     def test_get_uri(self):
-        self.assertTrue(module_dir in get_uri(module_dir))
+        self.assertTrue(MODULE_DIR in get_uri(MODULE_DIR))
 
     def test_get_database(self):
         d = {"host": "localhost", "port": 27017, "database": "atomate_unittest"}
 
         self.assertTrue(isinstance(get_database(settings=d), Database))
 
-        db = get_database(os.path.join(module_dir, "db.json"))
+        db = get_database(os.path.join(MODULE_DIR, "db.json"))
         self.assertTrue(isinstance(db, Database))
         self.assertEqual(db.client.address[0], "localhost")
         self.assertEqual(db.name, "atomate_unittest")
