17556e45004cfd41148a2d0c49cd83a7d5ffaa57
Brandon Wood <b.wood@berkeley.edu>
2018-05-21 13:16:56 -0700

    workflow tests added

---
+++ atomate/qchem/workflows/tests/test_parse_pass_write.py
@@ -0,0 +1,62 @@
+# coding: utf-8
+
+from __future__ import division, print_function, unicode_literals, absolute_import
+
+import os
+import unittest
+
+from atomate.qchem.firetasks.geo_transformations import RotateTorsion
+from atomate.qchem.firetasks.write_inputs import WriteInputFromIOSet
+from atomate.qchem.firetasks.parse_outputs import QChemToDb
+from fireworks import Firework, Workflow, FWorker
+from fireworks.core.rocket_launcher import rapidfire
+from atomate.utils.testing import AtomateTest
+from pymatgen.core import Molecule
+import numpy as np
+
+__author__ = 'Brandon Wood'
+__email__ = 'b.wood@berkeley.edu'
+
+module_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+db_dir = os.path.join(module_dir, "..", "..", "..", "common", "test_files")
+
+
+class TestParsePassWrite(AtomateTest):
+    @classmethod
+    def setUpClass(cls):
+
+        cls.pt_mol = Molecule.from_file(os.path.join(module_dir, "..", "..",
+                                                     "test_files", "pt_gs_wb97mv_tz_initial.xyz"))
+        cls.pt_rot_90_mol = Molecule.from_file(os.path.join(module_dir, "..", "..",
+                                                            "test_files", "pt_rotated_90.0.xyz"))
+
+    def setUp(self):
+        super(TestParsePassWrite, self).setUp()
+
+    def tearDown(self):
+        pass
+
+    def test_parse_pass_write(self):
+
+        input = "test.qin.opt_1"
+        output = "test.qout.opt_1"
+        calc_dir = os.path.join(module_dir, "..", "..", "test_files", "FF_working")
+
+        p_task = QChemToDb(calc_dir=calc_dir, input_file=input, output_file=output)
+        fw1 = Firework([p_task])
+        w_task = WriteInputFromIOSet(qchem_input_set="OptSet")
+        fw2 = Firework([w_task], parents=fw1)
+        wf = Workflow([fw1, fw2])
+
+        self.lp.add_wf(wf)
+        rapidfire(self.lp, fworker=FWorker(env={"db_file": os.path.join(db_dir, "db.json")}))
+
+
+        #ft = RotateTorsion({"molecule": self.pt_mol, "atom_indexes": atom_indexes, "angle": angle})
+        #rot_mol = ft.run_task({})
+        #test_mol = Molecule.from_dict(rot_mol.as_dict()["update_spec"]["prev_calc_molecule"])
+        #np.testing.assert_equal(self.pt_rot_90_mol.species, test_mol.species)
+        #np.testing.assert_allclose(self.pt_rot_90_mol.cart_coords, test_mol.cart_coords, atol=0.0001)
+
+if __name__ == '__main__':
+    unittest.main()
