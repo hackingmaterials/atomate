62741680f311f5bdfa244564c6d5289c8b01e9fe
samblau <samblau1@gmail.com>
2018-06-22 14:28:21 -0700

    Vastly improved fragmentation algorithm

--- atomate/qchem/firetasks/fragmenter.py
+++ atomate/qchem/firetasks/fragmenter.py
@@ -7,6 +7,8 @@ from __future__ import division, print_function, unicode_literals, absolute_impo
 
 from pymatgen.core.structure import Molecule
 from pymatgen.analysis.graphs import MoleculeGraph
+from itertools import combinations
+import networkx as nx
 from fireworks import FiretaskBase, explicit_serialize
 
 have_babel = True
@@ -56,92 +58,54 @@ class FragmentMolecule(FiretaskBase):
                 "No molecule present, add as an optional param or check fw_spec"
             )
         
+        # if edges are passed by the user
         if self.get("edges"):
             edges = self.get("edges")
+        # if not, and if we don't have babel, raise an error
         elif not have_babel:
             raise KeyError(
                 "OpenBabel not accessible and no bonds provided! Exiting..."
             )
+        # if using babel
         else:
             babel_mol = BabelMolAdaptor(mol).openbabel_mol
             edges = []
             for obbond in ob.OBMolBondIter(babel_mol):
                 edges += [[obbond.GetBeginAtomIdx()-1,obbond.GetEndAtomIdx()-1]]
 
+        # build the MoleculeGraph
         mol_graph = MoleculeGraph.with_empty_graph(mol)
         for edge in edges:
             mol_graph.add_edge(edge[0],edge[1])
+        mol_graph.graph = mol_graph.graph.to_undirected()
+        species = {}
+        for node in mol_graph.graph:
+            species[node] = mol_graph.molecule[node].specie.symbol
+        nx.set_node_attributes(mol_graph.graph, species, "specie")
 
-        done = False
-        self.all_unique_frags = []
+        # find all possible fragments, aka connected induced subgraphs
+        all_fragments = []
+        for ii in range(1,len(mol)+1):
+            for combination in combinations(mol_graph.graph.nodes,ii):
+                subgraph = nx.subgraph(mol_graph.graph, combination)
+                if nx.is_connected(subgraph):
+                    all_fragments.append(subgraph)
 
-        while not done:
-            if len(self.all_unique_frags) == 0:
-                frags = fragment(mol_graph)
-                new_frags_added = self.add_unique_frags(frags)
-            else:
-                total_new_frags = []
-                for ii,unique_frag in enumerate(self.all_unique_frags):
-                    subfrags = fragment(unique_frag)
-                    new_frags_added = self.add_unique_frags(subfrags)
-                    total_new_frags += new_frags_added
-                if len(total_new_frags) == 0:
-                    done = True
+        # narrow to all unique fragments by checking for isomorphism
+        unique_fragments = []
+        for fragment in all_fragments:
+            if not [is_isomorphic(fragment, f) for f in unique_fragments].count(True) >= 1:
+                unique_fragments.append(fragment)
+
+        self.all_unique_frags = unique_fragments
 
 
         # TODO insert into database
         # TODO how am I naming things for easy referencing?
 
-    def add_unique_frags(self, frags):
-        new_frags_added = []
-        for new_frag in frags:
-            is_new = True
-            for old_frag in self.all_unique_frags:
-                if new_frag.molecule.species == old_frag.molecule.species:
-                    if len(new_frag.molecule.species) == 1:
-                        is_new = is_new and False
-                    else:
-                        is_new = is_new and not new_frag.equivalent_to(old_frag)
-            if is_new:
-                self.all_unique_frags += [new_frag]
-                new_frags_added += [new_frag]
-        if DEBUG:
-            self.check_unique()
-        return new_frags_added
+def _node_match(node, othernode):
+    return node["specie"] == othernode["specie"]
 
-    def check_unique(self):
-        for ii in range(len(self.all_unique_frags)):
-            for jj in range(ii+1, len(self.all_unique_frags)):
-                if self.all_unique_frags[ii].molecule.species == self.all_unique_frags[jj].molecule.species:
-                    if self.all_unique_frags[ii].equivalent_to(self.all_unique_frags[jj]):
-                        print(self.all_unique_frags[ii])
-                        print(self.all_unique_frags[jj])
-                        raise ValueError("Equivalent fragments found! Exiting...")
-
-def fragment(mol_graph):
-    all_frags = []
-    ring_bonds = []
-    bad_pairs = []
-    for bond in mol_graph.graph.edges:
-        bonds = [(bond[0],bond[1])]
-        try:
-            frags = mol_graph.split_molecule_subgraphs(bonds,allow_reverse=True)
-            all_frags += frags
-        except RuntimeError:
-            ring_bonds += bonds
-
-    bond_pairs = []
-    for ii,bond in enumerate(ring_bonds):
-        for jj in range(ii+1,len(ring_bonds)):
-            bond_pair = [bond, ring_bonds[jj]]
-            bond_pairs += [bond_pair]
-
-    for bond_pair in bond_pairs:
-        try:
-            frags = mol_graph.split_molecule_subgraphs(bond_pair,allow_reverse=True)
-            all_frags += frags
-        except RuntimeError:
-            bad_pairs += bond_pair
-    return all_frags
-
-
+def is_isomorphic(graph1, graph2):
+    return nx.is_isomorphic(graph1, graph2, node_match=_node_match)
+    
