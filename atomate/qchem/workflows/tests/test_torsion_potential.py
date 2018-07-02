998338fa32a96148cd5ae4860c627416115ac122
samblau <samblau1@gmail.com>
2018-07-02 14:05:33 -0700

    Bug fix

--- atomate/qchem/workflows/tests/test_torsion_potential.py
+++ atomate/qchem/workflows/tests/test_torsion_potential.py
@@ -9,7 +9,7 @@ from fireworks import FWorker
 from fireworks.core.rocket_launcher import rapidfire
 from atomate.utils.testing import AtomateTest
 from pymatgen.core import Molecule
-from pymatgen.io.qchem_io.outputs import QCOutput
+from pymatgen.io.qchem.outputs import QCOutput
 from atomate.qchem.workflows.base.torsion_potential import get_wf_torsion_potential
 from atomate.qchem.powerups import use_fake_qchem
 from pymatgen.io.qchem.inputs import QCInput
