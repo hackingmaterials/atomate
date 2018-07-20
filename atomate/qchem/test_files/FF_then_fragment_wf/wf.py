d06efbb128730338cbc2602065f92752e6dd710a
samblau <samblau1@gmail.com>
2018-07-19 17:27:03 -0700

    Switching to easier test system

--- atomate/qchem/test_files/FF_then_fragment_wf/wf.py
+++ atomate/qchem/test_files/FF_then_fragment_wf/wf.py
@@ -6,7 +6,7 @@ from atomate.qchem.workflows.base.FF_then_fragment import get_wf_FF_then_fragmen
 from fireworks.core.launchpad import LaunchPad
 from pymatgen.core import Molecule
 
-mol = Molecule.from_file("PC.xyz")
+mol = Molecule.from_file("BF4-.xyz")
 wf = get_wf_FF_then_fragment(molecule=mol, max_cores=32)
 lp = LaunchPad.auto_load()
 lp.add_wf(wf)
