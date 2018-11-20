6fb6c98030f05082c0139688d571fec22f3fdcb7
samblau <samblau1@gmail.com>
2018-11-20 11:23:06 -0800

    Bug fix

--- atomate/qchem/firetasks/parse_outputs.py
+++ atomate/qchem/firetasks/parse_outputs.py
@@ -67,7 +67,7 @@ class QChemToDb(FiretaskBase):
         # parse the QChem directory
         logger.info("PARSING DIRECTORY: {}".format(calc_dir))
 
-        additional_fields = self.get("additional_fields")
+        additional_fields = self.get("additional_fields", [])
 
         drone = QChemDrone(additional_fields=additional_fields)
 
@@ -85,15 +85,20 @@ class QChemToDb(FiretaskBase):
         # Update fw_spec with final/optimized structure
         update_spec = {}
         if task_doc.get("output").get("optimized_molecule"):
-            if additional_fields["task_label"][0:7] == "ion_pos":
-                tmp = {}
-                tmp["molecule"] = task_doc["output"]["optimized_molecule"]
-                tmp["mulliken"] = task_doc["output"]["mulliken"]
-                tmp["energy"] = task_doc["output"]["final_energy"]
-                tmp["calc_dir"] = calc_dir
-                tmp["linked"] = task_doc["linked"]
-                update_spec[additional_fields["task_label"]] = tmp
-            else:
+            is_ion_pos = False
+            if "task_label" in additional_fields:
+                if len(additional_fields["task_label"]) > 7:
+                    if additional_fields["task_label"][0:7] == "ion_pos":
+                        is_ion_pos = True
+                        tmp = {}
+                        tmp["molecule"] = task_doc["output"]["optimized_molecule"]
+                        tmp["mulliken"] = task_doc["output"]["mulliken"]
+                        tmp["energy"] = task_doc["output"]["final_energy"]
+                        tmp["calc_dir"] = calc_dir
+                        tmp["linked"] = task_doc["linked"]
+                        # add rem, solvent?
+                        update_spec[additional_fields["task_label"]] = tmp
+            if not is_ion_pos:
                 update_spec["prev_calc_molecule"] = task_doc["output"]["optimized_molecule"]
                 update_spec["prev_calc_mulliken"] = task_doc["output"]["mulliken"]
             
