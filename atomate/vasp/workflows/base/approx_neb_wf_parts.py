76d30781c66506fd4d857ceaa73e7bb670b6a1a8
acrutt <acrutt@lbl.gov>
2019-06-14 14:15:42 -0700

    Add HostLatticeToDb Firetask (1st draft)

--- atomate/vasp/workflows/base/approx_neb_wf_parts.py
+++ atomate/vasp/workflows/base/approx_neb_wf_parts.py
@@ -10,118 +10,149 @@ from pymatgen.io.vasp.sets import MPRelaxSet
 from pymatgen import Structure, Element
 
 
-#TODO: Write Firetask for taking CHGCAR from previous directory and storing it in database
+# TODO: Write Firetask for taking CHGCAR from previous directory and storing it in database
 @explicit_serialize
-class ChgDensityToDb(FiretaskBase):
+class HostLatticeToDb(FiretaskBase):
     """
-    Enter the CHGCAR or AECCAR from a previous run into a database.
-    Uses current directory unless you specify the previous calculation using calc_dir or calc_loc.
-    Adapted from VaspToDb Firetask but only stores charge density data.
+    Initializes a approx_neb database entry from a host lattice task doc
+    specified by the supplied task_id.
+    Generates a unique id (wf_id) for approx_neb workflow record keeping.
+    Host lattice task doc is updated with this unique approx_neb workflow id.
+    Pulls GridFS identifier (or parses and stores from files in task doc
+    directory if not already stored) for accessing host lattice CHGCAR and AECCARs.
 
     Required params:
         db_file (str): path to file containing the database credentials.
-        parse_chgcar (bool):
-        parse_aeccar (bool):
-    Optional params:
-        calc_dir (str): path to dir (on current filesystem) that contains VASP
-            output files. Default: use current working directory.
-        calc_loc (str OR bool): if True will set most recent calc_loc. If str
-            search for the most recent calc_loc with the matching name
-        additional_fields (dict): dict of additional fields to add
-        fw_spec_field (str): if set, will update the task doc with the contents
-            of this key in the fw_spec.
-        defuse_unsuccessful (bool): this is a three-way toggle on what to do if
-            your job looks OK, but is actually unconverged (either electronic or
-            ionic). True -> mark job as COMPLETED, but defuse children.
-            False --> do nothing, continue with workflow as normal. "fizzle"
-            --> throw an error (mark this job as FIZZLED)
-        task_fields_to_push (dict): if set, will update the next Firework/Firetask
-            spec using fields from the task document.
-            Format: {key : path} -> fw.spec[key] = task_doc[path]
-            The path is a full mongo-style path so subdocuments can be referneced
-            using dot notation and array keys can be referenced using the index.
-            E.g "calcs_reversed.0.output.outar.run_stats"
+        host_lattice_task_id (int): task_id for structure optimization of host lattice
     """
-    required_params = ["db_file", "parse_chgcar", "parse_aeccar"]
-    optional_params = ["calc_dir", "calc_loc",
-                       "additional_fields", "fw_spec_field", "defuse_unsuccessful",
-                       "task_fields_to_push"]
+
+    required_params = ["db_file", "host_lattice_task_id"]
+    optional_params = []
 
     def run_task(self, fw_spec):
-        # get the directory that contains the previous VASP calculation for storing CHGCAR and/or AECCAR
-        calc_dir = os.getcwd()
-        if "calc_dir" in self:
-            calc_dir = self["calc_dir"]
-        elif self.get("calc_loc"):
-            calc_dir = get_calc_loc(self["calc_loc"], fw_spec["calc_locs"])["path"]
-
-        # parse the VASP directory
-        logger.info("PARSING DIRECTORY: {}".format(calc_dir))
-
-        drone = VaspDrone(additional_fields=self.get("additional_fields"),
-                          parse_chgcar=fw_spec["parse_chgcar"],
-                          parse_aeccar=fw_spec["parse_aeccar"])
-
-        # assimilate (i.e. parse)
-        task_doc = drone.assimilate(calc_dir)
-
-        # Check for additional keys to set based on the fw_spec
-        if self.get("fw_spec_field"):
-            task_doc.update(fw_spec[self.get("fw_spec_field")])
-
         # get the database connection
-        db_file = fw_spec["db_file"] #env_chk(self.get('db_file'), fw_spec)
+        db_file = env_chk(self.get("db_file"), fw_spec["db_file"])
+        mmdb = VaspCalcDb.from_db_file(db_file, admin=True)
+        wf_uuid = str(uuid4())
 
-        # db insertion
-        if db_file:
-            mmdb = VaspCalcDb.from_db_file(db_file, admin=True)
-            #the chgcar
-            if fw_spec["parse_chgcar"]:
-                task_doc["calcs_reversed"][0]["chgcar"]
+        # get host lattice task doc from host_lattice_task_id
+        t_id = self["host_lattice_task_id"]
+        try:
+            host_lattice_tasks_doc = mmdb.collection.find_one(
+                {"task_id": t_id, "approx_neb.calc_type": "host_lattice"}
+            )
+        except:
+            raise ValueError(
+                "Unable to find approx neb host lattice with task_id: {}".format(t_id)
+            )
+        # update found host lattice task doc with unique wf_uuid
+        # (tracks approx_neb workflows generated from this host lattice task doc)
+        mmdb.collection.update_one(
+            {"task_id": t_id}, {"$push": {"approx_neb.wf_uuids": wf_uuid}}
+        )
 
-            if fw_spec["parse_aeccar"]:
-                task_doc["calcs_reversed"][0]["aeccar0"]
-                task_doc["calcs_reversed"][0]["aeccar2"]
+        # Initialize and store select host lattice task doc fields in approx_neb_doc
+        # (to be stored in the approx_neb collection)
+        approx_neb_doc = {
+            "host_lattice": {
+                "dir_name": host_lattice_tasks_doc["dir_name"],
+                "formula_pretty": host_lattice_tasks_doc["formula_pretty"],
+                "output": host_lattice_tasks_doc["output"],
+                "task_id": host_lattice_tasks_doc["task_id"],
+            },
+            "wf_uuid": wf_uuid,
+        }
 
-            t_id = mmdb.insert_task(
-                task_doc, self.get("parse_chgcar", False)
-                or self.get("parse_aeccar", False))
-            logger.info("Finished parsing with task_id: {}".format(t_id))
+        # Gets GridFS ids for host lattice chgcar and aeccar if stored in task_doc
+        # gridfs_ids is for record keeping to track the source of GridFS ids
+        chgcar_fs_id = host_lattice_tasks_doc["calcs_reversed"][0].get("chgcar_fs_id")
+        aeccar0_fs_id = host_lattice_tasks_doc["calcs_reversed"][0].get("aeccar0_fs_id")
+        aeccar2_fs_id = host_lattice_tasks_doc["calcs_reversed"][0].get("aeccar2_fs_id")
+        gridfs_ids = {
+            "task_doc_chgcar": chgcar_fs_id,
+            "task_doc_aeccar0": aeccar0_fs_id,
+            "task_doc_aeccar2": aeccar2_fs_id,
+        }
 
-        defuse_children = False
-        if task_doc["state"] != "successful":
-            defuse_unsuccessful = self.get("defuse_unsuccessful",
-                                           DEFUSE_UNSUCCESSFUL)
-            if defuse_unsuccessful is True:
-                defuse_children = True
-            elif defuse_unsuccessful is False:
-                pass
-            elif defuse_unsuccessful == "fizzle":
-                raise RuntimeError(
-                    "ChgDensityToDb indicates that job is not successful "
-                    "(perhaps your job did not converge within the "
-                    "limit of electronic/ionic iterations)!")
-            else:
-                raise RuntimeError("Unknown option for defuse_unsuccessful: "
-                                   "{}".format(defuse_unsuccessful))
+        # If unable to get GridFS ids, checks task doc directory for
+        # CHGCAR and/or AECCAR files. Stores files in database via GridFS
+        # and updates host lattice task doc with fs_id
+        if any(fs_id == None for fs_id in [chgcar_fs_id, aeccar0_fs_id, aeccar2_fs_id]):
+            calc_dir = approx_neb_doc["host_lattice"]["dir_name"]
+            logger.info("CHECKING FOR CHG DENSITY FILES: {}".format(calc_dir))
+            drone = VaspDrone(parse_chgcar=True, parse_aeccar=True)
+            task_doc = drone.assimilate(calc_dir)
+            # insert chgcar with GridFS
+            if chgcar_fs_id == None:
+                chgcar = json.dumps(
+                    task_doc["calcs_reversed"][0]["chgcar"], cls=MontyEncoder
+                )
+                chgcar_gfs_id, compression_type = mmdb.insert_gridfs(
+                    chgcar, "chgcar_fs", task_id=t_id
+                )
+                mmdb.collection.update_one(
+                    {"task_id": t_id},
+                    {
+                        "$set": {
+                            "calcs_reversed.0.chgcar_compression": compression_type,
+                            "calcs_reversed.0.chgcar_fs_id": chgcar_gfs_id,
+                        }
+                    },
+                )
+                gridfs_ids["dir_chgcar"] = chgcar_gfs_id
+                logger.info("CHGCAR GRIDFS INSERTION COMPLETE: {}".format(calc_dir))
+            # insert aeccar with GridFS
+            if aeccar0_fs_id == None or aeccar2_fs_id == None:
+                aeccar0 = task_doc["calcs_reversed"][0]["aeccar0"]
+                aeccar2 = task_doc["calcs_reversed"][0]["aeccar2"]
+                # check if the aeccar is valid before insertion
+                if (aeccar0.data["total"] + aeccar2.data["total"]).min() < 0:
+                    logger.warning(
+                        "AECCARs appear corrupted for task id {t_id} in {calc_dir}\nSkipping GridFS storage of AECCARs"
+                    )
+                else:
+                    aeccar0_gfs_id, compression_type = mmdb.insert_gridfs(
+                        aeccar0, "aeccar0_fs", task_id=t_id
+                    )
+                    mmdb.collection.update_one(
+                        {"task_id": t_id},
+                        {
+                            "$set": {
+                                "calcs_reversed.0.aeccar0_compression": compression_type,
+                                "calcs_reversed.0.aeccar0_fs_id": aeccar0_gfs_id,
+                            }
+                        },
+                    )
+                    gridfs_ids["dir_aeccar0"] = aeccar0_gfs_id
+                    aeccar2_gfs_id, compression_type = mmdb.insert_gridfs(
+                        aeccar2, "aeccar2_fs", task_id=t_id
+                    )
+                    mmdb.collection.update_one(
+                        {"task_id": t_id},
+                        {
+                            "$set": {
+                                "calcs_reversed.0.aeccar2_compression": compression_type,
+                                "calcs_reversed.0.aeccar2_fs_id": aeccar2_gfs_id,
+                            }
+                        },
+                    )
+                    gridfs_ids["dir_aeccar2"] = aeccar2_gfs_id
+                    logger.info("AECCAR GRIDFS INSERTION COMPLETE: {}".format(calc_dir))
 
-        task_fields_to_push = self.get("task_fields_to_push", None)
-        update_spec = {}
-        if task_fields_to_push:
-            if isinstance(task_fields_to_push, dict):
-                for key, path_in_task_doc in task_fields_to_push.items():
-                    if has(task_doc, path_in_task_doc):
-                        update_spec[key] = get(task_doc, path_in_task_doc)
-                    else:
-                        logger.warning("Could not find {} in task document. Unable to push to next firetask/firework".format(path_in_task_doc))
-            else:
-                raise RuntimeError("Inappropriate type {} for task_fields_to_push. It must be a "
-                                   "dictionary of format: {key: path} where key refers to a field "
-                                   "in the spec and path is a full mongo-style path to a "
-                                   "field in the task document".format(type(task_fields_to_push)))
+        # Store GridFS ids in approx_neb_doc (to be stored in the approx_neb collection)
+        approx_neb_doc["chgcar_fs_id"] = chgcar_fs_id or chgcar_gfs_id
+        approx_neb_doc["aeccar0_fs_id"] = aeccar0_fs_id or aeccar0_gfs_id
+        approx_neb_doc["aeccar2_fs_id"] = aeccar2_fs_id or aeccar2_gfs_id
+        # Insert approx_neb_doc in the approx_neb collection of provided database
+        mmdb.collection = mmdb.db["approx_neb"]
+        mmdb.collection.insert_one(approx_neb_doc)
+        # Update fw_spec with approx_neb_doc and
+        # store wf_uuid and gridfs_ids in launches collection for record keeping
+        return FWAction(
+            stored_data={"wf_uuid": wf_uuid, "gridfs_ids": gridfs_ids},
+            update_spec=approx_neb_doc,
+        )
 
-        return FWAction(stored_data={"task_id": task_doc.get("task_id", None)},
-                        defuse_children=defuse_children, update_spec=update_spec)
 
 @explicit_serialize
 class InsertSites(FiretaskBase):
@@ -142,9 +173,7 @@ class InsertSites(FiretaskBase):
         insert_coords = self["insert_coords"]
         insert_specie = self["insert_specie"]
 
-        if (
-            structure.site_properties != {}
-        ):  # removes site properties to avoid error
+        if structure.site_properties != {}:  # removes site properties to avoid error
             for p in structure.site_properties.keys():
                 structure.remove_site_property(p)
         for coords in sorted(insert_coords, reverse=True):
@@ -189,20 +218,10 @@ class InsertSitesFW(Firework):
             structure, **override_default_vasp_params
         )
         t = [pass_structure_fw]
-        t.append(
-            WriteVaspFromIOSet(
-                structure=structure, vasp_input_set=vasp_input_set
-            )
-        )
-        t.append(
-            RunVaspCustodian(
-                vasp_cmd=vasp_cmd, job_type="double_relaxation_run"
-            )
-        )
+        t.append(WriteVaspFromIOSet(structure=structure, vasp_input_set=vasp_input_set))
+        t.append(RunVaspCustodian(vasp_cmd=vasp_cmd, job_type="double_relaxation_run"))
         t.append(PassCalcLocs(name=name))
-        t.append(
-            VaspToDb(db_file=db_file, additional_fields={"task_label": name})
-        )
+        t.append(VaspToDb(db_file=db_file, additional_fields={"task_label": name}))
 
 
 class PathFinderFW(Firework):
@@ -226,14 +245,10 @@ class PathFinderFW(Firework):
         t = []
         if prev_calc_dir:
             t.append(
-                CopyVaspOutputs(
-                    calc_dir=prev_calc_dir, additional_files=["CHGCAR"]
-                )
+                CopyVaspOutputs(calc_dir=prev_calc_dir, additional_files=["CHGCAR"])
             )
         elif parents:
-            t.append(
-                CopyVaspOutputs(calc_loc=True, additional_files=["CHGCAR"])
-            )
+            t.append(CopyVaspOutputs(calc_loc=True, additional_files=["CHGCAR"]))
         else:
             raise ValueError(
                 "Must specify previous calculation to use CHGCAR for PathfinderFW"
@@ -243,15 +258,9 @@ class PathFinderFW(Firework):
         task_name = name + "???"
         t.append(RunVaspCustodian(vasp_cmd=vasp_cmd, auto_npar=">>auto_npar<<"))
         t.append(PassCalcLocs(name=task_name))
-        t.append(
-            VaspToDb(
-                db_file=db_file, additional_fields={"task_label": task_name}
-            )
-        )
+        t.append(VaspToDb(db_file=db_file, additional_fields={"task_label": task_name}))
 
-    def add_fix_two_atom_selective_dynamics(
-        structure, fixed_index, fixed_specie
-    ):
+    def add_fix_two_atom_selective_dynamics(structure, fixed_index, fixed_specie):
         """
         Returns structure with selective dynamics assigned to fix the
         position of two sites.
