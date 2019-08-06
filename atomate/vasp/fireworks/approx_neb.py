7dc9a9cb79fcb9aad8efdad70b0fc7ca04c2d982
acrutt <acrutt@lbl.gov>
2019-08-06 14:08:12 -0700

    Created StableSiteFW to streamline site insertion and running VASP to relax the subsequent (inserted) structure.

--- atomate/vasp/fireworks/approx_neb.py
+++ atomate/vasp/fireworks/approx_neb.py
@@ -338,3 +338,115 @@ class GetImagesFW(Firework):
 
         super().__init__(tasks=t, name=name, parents=parents, **kwargs)
 
+
+class StableSiteFW(Firework):
+    def __init__(
+        self,
+        approx_neb_wf_uuid,
+        insert_specie,
+        insert_coords,
+        stable_sites_index,
+        name="stable site",
+        db_file=DB_FILE,
+        vasp_input_set=None,
+        vasp_cmd=VASP_CMD,
+        override_default_vasp_params=None,
+        job_type="double_relaxation_run",
+        parents=None,
+        **kwargs
+    ):
+        """
+        ToDo: Update description
+        Updates the fw_spec with the empty host lattice task_id from the provided
+        approx_neb_wf_uuid. Pulls the empty host lattice structure from the tasks
+        collection and inserts the site(s) designated by insert_specie and
+        insert_coords. Stores the modified structure in the stable_sites field of
+        the approx_neb collection.
+        Launches a structure optimization calculation from a structure stored in
+        in the approx_neb collection. Structure input for calculation is specified
+        by the provided approx_neb_wf_uuid and stable_sites_index to pull the
+        structure from the approx_neb collection using pydash.get().
+
+        Adapted from OptimizeFW.
+
+        Args:
+            approx_neb_wf_uuid (str): Unique identifier for approx workflow record
+                keeping.
+            insert_specie (str): specie of site to insert in structure (e.g. "Li")
+            insert_coords (1x3 array or list of 1x3 arrays): coordinates of site(s)
+                to insert in structure (e.g. [0,0,0] or [[0,0,0],[0,0.25,0]])
+            stable_sites_index (int): index used in stable_sites field of
+                approx_neb collection for workflow record keeping
+            name (str): Combined with insert_specie and stable_sites_index to label the firework
+            vasp_input_set (VaspInputSet): input set to use. Defaults to
+                MPRelaxSet() if None.
+            override_default_vasp_params (dict): If this is not None, these params
+                are passed to the default vasp_input_set, i.e., MPRelaxSet. This
+                allows one to easily override some settings (e.g.
+                user_incar_settings, etc.)
+            vasp_cmd (str): Command to run vasp.
+            db_file (str): Path to file specifying db credentials to store outputs.
+            job_type (str): custodian job type (default "double_relaxation_run")
+
+            parents ([Firework]): Parents of this particular Firework.
+            \*\*kwargs: Other kwargs that are passed to Firework.__init__.
+        """
+        fw_name = name + ": insert " + insert_specie + " " + str(stable_sites_index)
+
+        # set additional_fields to be added to task doc by VaspToDb
+        # initiates the information stored in the tasks collection to aid record keeping
+        additional_fields = {
+            "task_label": fw_name,
+            "approx_neb": {
+                "wf_uuids": [],
+                "_source_wf_uuid": approx_neb_wf_uuid,
+                "calc_type": "stable_site",
+                "stable_sites_indexes": []
+            }
+        }
+
+        t = []
+        # Add structure_task_id (for empty host lattice) to fw_spec
+        # structure_task_id is required for InsertSites firetask
+        t.append(
+            PassFromDb(
+                db_file=db_file,
+                approx_neb_wf_uuid=approx_neb_wf_uuid,
+                fields_to_pull={"structure_task_id": "host_lattice.task_id"},
+            )
+        )
+        # Insert sites into empty host lattice (specified by structure_task_id)
+        t.append(
+            InsertSites(
+                db_file=db_file,
+                insert_specie=insert_specie,
+                insert_coords=insert_coords,
+                stable_sites_index=stable_sites_index,
+                approx_neb_wf_uuid=approx_neb_wf_uuid,
+            )
+        )
+        # write vasp inputs, run vasp, parse vasp outputs
+        t.append(
+            WriteVaspInput(
+                db_file=db_file,
+                approx_neb_wf_uuid=approx_neb_wf_uuid,
+                vasp_input_set=vasp_input_set,
+                structure_path="stable_sites." + str(stable_sites_index) + ".output.structure",
+                override_default_vasp_params=override_default_vasp_params,
+            )
+        )
+        t.append(RunVaspCustodian(vasp_cmd=vasp_cmd, job_type=job_type))
+        t.append(PassCalcLocs(name=name))
+        t.append(
+            VaspToDb(
+                db_file=db_file,
+                additional_fields=additional_fields,
+                task_fields_to_push={"stable_site_task_id": "task_id"},
+            )
+        )
+        # store desired outputs from tasks doc in approx_neb collection
+        t.append(
+            StableSiteToDb(db_file=db_file, approx_neb_wf_uuid=approx_neb_wf_uuid)
+        )
+
+        super().__init__(tasks=t, name=fw_name, parents=parents, **kwargs)
