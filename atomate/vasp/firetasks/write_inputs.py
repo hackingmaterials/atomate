c1c6fa27f2765f48660e7d8b6a3e87675487163a
zyrd <shyuep@gmail.com>
2019-09-05 09:41:37 -0700

    more __future__ removals.

--- atomate/vasp/firetasks/write_inputs.py
+++ atomate/vasp/firetasks/write_inputs.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 
-from __future__ import division, print_function, unicode_literals, absolute_import
 
 """
 This module defines tasks for writing vasp input sets for various types of vasp calculations
