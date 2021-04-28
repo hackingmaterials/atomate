0a893c60373bcd2f7d784b44f19bce7f6558f8e6
acrutt <acrutt@lbl.gov>
2021-04-28 10:26:32 -0700

    TestApproxNEBWorkflow.test_wf() debugging

--- atomate/vasp/workflows/tests/test_approx_neb_workflow.py
+++ atomate/vasp/workflows/tests/test_approx_neb_workflow.py
@@ -4,7 +4,6 @@ from pymatgen.core import Structure
 from atomate.vasp.workflows.base.approx_neb import get_aneb_wf
 from fireworks import FWorker
 from fireworks.core.rocket_launcher import rapidfire
-from time import sleep
 
 __author__ = "Ann Rutt"
 __email__ = "acrutt@lbl.gov"
@@ -65,12 +64,6 @@ class TestApproxNEBWorkflow(AtomateTest):
         fw_ids = self.lp.add_wf(self.wf)
         rapidfire(self.lp, fworker=FWorker(env={"db_file": db_dir / "db.json"}))
 
-        sleep(60)
-
-        # 3 images fws are added after running the workflow
-        run_wf = self.lp.get_wf_by_fw_id(list(fw_ids.values())[0])
-        self.assertEqual(len(run_wf.fws), 7)
-
         # check task docs
         host_tds = list(
             self.get_task_collection().find(
@@ -117,11 +110,11 @@ class TestApproxNEBWorkflow(AtomateTest):
         for i in aneb_doc["images"]["0+1"]:
             self.assertIn("output", i)
 
+        # 3 images fws are added after running the workflow
+        run_wf = self.lp.get_wf_by_fw_id(list(fw_ids.values())[0])
+        self.assertEqual(len(run_wf.fws), 7)
+
         # check workflow finished without error
         is_completed = [s == "COMPLETED" for s in run_wf.fw_states.values()]
         self.assertTrue(all(is_completed))
         self.assertEqual(len(is_completed), 7)
-
-        # 3 images fws are added after running the workflow
-        run_wf = self.lp.get_wf_by_fw_id(list(fw_ids.values())[0])
-        self.assertEqual(len(run_wf.fws), 7)
