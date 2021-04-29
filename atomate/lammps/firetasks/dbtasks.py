23b44ccee880c71603f51b4e8924166b5efb50d2
Unknown <sivonxay@gmail.com>
2021-04-29 12:32:04 -0700

    cast dict keys to list

--- atomate/lammps/firetasks/dbtasks.py
+++ atomate/lammps/firetasks/dbtasks.py
@@ -43,7 +43,7 @@ class LammpsMDToDB(FiretaskBase):
             for group_str in groups:
                 atom, _str, atom_id = group_str.split()
                 atomic_map[int(atom_id)] = atom
-        elif isinstance(atomic_map.keys()[0], str):
+        elif isinstance(list(atomic_map.keys())[0], str):
             _atomic_map = {}
             for i in atomic_map:
                 _atomic_map[int(i)] = atomic_map[i]
