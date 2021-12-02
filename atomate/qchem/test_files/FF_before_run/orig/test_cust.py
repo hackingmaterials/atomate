0d1203d0c64cfc289c94fd51300255b7f06e8117
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-12-02 11:11:13 +0000

    pre-commit run black -a

--- atomate/qchem/test_files/FF_before_run/orig/test_cust.py
+++ atomate/qchem/test_files/FF_before_run/orig/test_cust.py
@@ -5,10 +5,17 @@ from custodian.qchem.new_handlers import QChemErrorHandler
 my_input = "test.qin"
 my_output = "test.qout"
 
-myjob = QCJob.opt_with_frequency_flattener(qchem_command="qchem -slurm",multimode="openmp",input_file=my_input,output_file=my_output,max_iterations=10,max_molecule_perturb_scale=0.3,max_cores=12)
-myhandler = QChemErrorHandler(input_file=my_input,output_file=my_output)
+myjob = QCJob.opt_with_frequency_flattener(
+    qchem_command="qchem -slurm",
+    multimode="openmp",
+    input_file=my_input,
+    output_file=my_output,
+    max_iterations=10,
+    max_molecule_perturb_scale=0.3,
+    max_cores=12,
+)
+myhandler = QChemErrorHandler(input_file=my_input, output_file=my_output)
 
-c = Custodian([myhandler],myjob,max_errors_per_job=10,max_errors=10)
+c = Custodian([myhandler], myjob, max_errors_per_job=10, max_errors=10)
 
 c.run()
-
--- atomate/qchem/test_files/real_run/test_cust.py
+++ atomate/qchem/test_files/real_run/test_cust.py
@@ -5,10 +5,15 @@ from custodian.qchem.handlers import QChemErrorHandler
 my_input = "mol.qin"
 my_output = "mol.qout"
 
-myjob = QCJob(qchem_command="qchem -slurm",multimode="openmp",input_file=my_input,output_file=my_output,max_cores=12)
-myhandler = QChemErrorHandler(input_file=my_input,output_file=my_output)
+myjob = QCJob(
+    qchem_command="qchem -slurm",
+    multimode="openmp",
+    input_file=my_input,
+    output_file=my_output,
+    max_cores=12,
+)
+myhandler = QChemErrorHandler(input_file=my_input, output_file=my_output)
 
-c = Custodian([myhandler],[myjob],max_errors_per_job=10,max_errors=10)
+c = Custodian([myhandler], [myjob], max_errors_per_job=10, max_errors=10)
 
 c.run()
-
