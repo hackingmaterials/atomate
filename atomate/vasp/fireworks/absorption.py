8e58aedf7f813dfe8317b43a04fafaf246c0bcf0
yang-ruoxi <roxie@Live.cn>
2021-12-07 20:24:11 -0800

    add absorptionFW

--- atomate/vasp/fireworks/absorption.py
+++ atomate/vasp/fireworks/absorption.py
@@ -39,40 +39,39 @@ class AbsorptionFW(Firework):
             db_file=DB_FILE,
             vasptodb_kwargs=None,
             parents=None,
-            vasp_input_set_params={},
+            vasp_input_set_params=None,
             **kwargs,
     ):
         """
-        FW that calculates frequency dpendent dielectric function within
+        FW that calculates frequency dependent dielectric function within
         indenpendent-particle-approxiamtion. A previous ground state calculation
-        with WAVECAR is required by specifying mode = 'static'; in the case of no
+        with the output WAVECAR is required by specifying mode = 'static'; in the case of no
         parent, a PBE functional ground state calculation will be performed and
-        the WAVECAR will be saved. Then another calculation with  'ALGO = EXACT, LOPTICS = True'
-        with variable NBANDS will be performed (MODE = "IPA"). This calculation
-        can save the WAVECAR and WAVEDER in case one wants to run RPA level absorption
+        the WAVECAR will be saved. Then, perform another calculation with  'ALGO = EXACT, LOPTICS = True'
+        with variable NBANDS by specifying MODE = "IPA". This calculation will save the
+        WAVECAR and WAVEDER in case one wants to run RPA level absorption
         spectra. For RPA-DFT absorption spectrum, run another mode = 'RPA' calculation
         with the WAVECAR, WAVEDER saved from previous IPA calc.
         Args:
             structure (Structure): Input structure. For an interpolation, this
                 is a dummy structure. See interpolate arg description.
-            name (str): Name for the polarization FireWork.
-            static_name (str): Name for the SCF run to be used in PassCalcLoc
-                if copy_vasp_outputs != True.
-            mode: 'STATIC', 'IPA', or 'RPA'. Default is 'IPA'
+            name (str): Name for the FireWork.
+            mode: 'STATIC', 'IPA', or 'RPA'.
+            nbands: number of bands to use, leave to None, and use nbands_factor instead
+            nbands_factor: the multiplication of the number of bands
+            reciprocal_density: k-point density
+            nkred: reduced number of k-points, for RPA calculation use only, reduces the computing time
+            nedos: energy mesh for DOS
             vasp_cmd (str): Command to run vasp.
+            prev_calc_loc (bool or str): If true (default), copies outputs from previous calc. If
+                a str value, retrieves a previous calculation output by name.
             vasp_input_set (str): string name for the VASP input set (e.g.,
                 "MPAbsorptionSet").
-            vasp_input_set_params (dict): Dict of vasp_input_set_kwargs.
             db_file (str): Path to file specifying db credentials.
             parents (Firework): Parents of this particular Firework. FW or list
                 of FWS.
-            vasp_cmd (str): Command to run vasp.
-            prev_calc_loc (bool or str): If true (default), copies outputs from previous calc. If
-                a str value, retrieves a previous calculation output by name. If False/None, will create
-                new SCAN calculation using the provided structure.
+            vasp_input_set_params (dict): Dict of vasp_input_set_kwargs.
             prev_calc_dir (str): Path to a previous calculation to copy from
-            db_file (str): Path to file specifying db credentials.
-            parents (Firework): Parents of this particular Firework. FW or list of FWs.
             vasptodb_kwargs (dict): kwargs to pass to VaspToDb
             **kwargs: Other kwargs that are passed to Firework.__init__.
 
@@ -88,8 +87,6 @@ class AbsorptionFW(Firework):
         fw_name = "{}-{}-{}".format(
             structure.composition.reduced_formula if structure else "unknown", name, mode
         )
-
-        has_previous_calc = False
 
         # define what wavecars to copy from the previous run
         if mode == "STATIC":
@@ -157,14 +154,14 @@ class AbsorptionFW(Firework):
         # when mode = "static"
         elif mode == "STATIC":
             if prev_calc_dir:
-                # Copy only the CONTCAR from previous calc directory
+                # Copy only the CONTCAR from previous calc directory (often a relaxation run)
                 t.append(CopyVaspOutputs(
                     calc_dir=prev_calc_dir,
                     contcar_to_poscar=True,
                     additional_files=wavecars)
                 )
 
-                t.append(WriteVaspStaticFromPrev(user_incar_settings={"LWAVE": "TRUE"}))
+                t.append(WriteVaspStaticFromPrev(other_params = {"user_incar_settings": {"LWAVE": "TRUE"}}))
 
             elif parents:
                 # Copy only the CONTCAR from previous calc
@@ -176,7 +173,8 @@ class AbsorptionFW(Firework):
                     )
                 )
 
-                t.append(WriteVaspStaticFromPrev(user_incar_settings={"LWAVE": "TRUE"}))
+                t.append(
+                    WriteVaspStaticFromPrev(other_params = {"user_incar_settings": {"LWAVE": "TRUE"}}))
 
             elif structure:
                 vasp_input_set = MPStaticSet(
@@ -187,10 +185,10 @@ class AbsorptionFW(Firework):
                 )
 
             else:
-                raise ValueError("Must specify structure or previous calculation for Static calculation")
+                raise ValueError("Must specify structure or previous calculation for static calculation")
 
         else:
-            raise ValueEroor("Must specify mode from 'STATIC', 'IPA', or 'RPA'")
+            raise ValueEroor("Must specify a mode from 'STATIC', 'IPA', or 'RPA'")
 
         # use the 'default' custodian handler group
         handler_group = "default"
