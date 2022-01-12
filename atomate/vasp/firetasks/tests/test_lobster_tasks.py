0473b0543d1508be03e76e361f5192523b26c18d
Zhuoying Zhu <zhz004@eng.ucsd.edu>
2022-01-11 18:41:28 -0800

    Update test_lobster_tasks.py

--- atomate/vasp/firetasks/tests/test_lobster_tasks.py
+++ atomate/vasp/firetasks/tests/test_lobster_tasks.py
@@ -157,12 +157,12 @@ class TestLobsterRunToDb(AtomateTest):
         self.assertListEqual(load_dict["output"]["chargespilling"], [0.008, 0.008])
         db = self.get_task_database()
         gfs = gridfs.GridFS(db, "lobster_files")
-        results = gfs.find({}).count_documents()
-        self.assertEqual(results, 2)
+        results = gfs.find({}).read()
+        self.assertEqual(len(results), 2)
         for fn in ["ICOHPLIST", "COOPCAR"]:
             oid = load_dict[fn.lower() + "_id"]
-            results = gfs.find({"_id": oid}).count_documents()
-            self.assertEqual(results, 1)
+            results = gfs.find({"_id": oid}).read()
+            self.assertEqual(len(results), 1)
 
     def test_jsonfile_si(self):
         copy_r(self.vasp_si_dir, ".")
