0fd7db503a715fbd3d88501164c63384ad87a1ba
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-01-25 21:25:12 +0000

    use pytest-split in GHA test workflow

---
+++ .github/workflows/combine_durations.py
@@ -0,0 +1,27 @@
+import json
+from pathlib import Path
+
+split_prefix = "split-"
+durations_path = Path(".test_durations")
+
+split_paths = Path(".").glob(f"{split_prefix}*/{durations_path.name}")
+
+try:
+    previous_durations = json.loads(durations_path.read_text())
+except FileNotFoundError:
+    previous_durations = {}
+
+new_durations = previous_durations.copy()
+
+for path in split_paths:
+    durations = json.loads(path.read_text())
+    new_durations.update(
+        {
+            key: duration
+            for key, duration in durations.items()
+            if previous_durations[key] != duration
+        }
+    )
+
+durations_path.parent.mkdir(parents=True, exist_ok=True)
+durations_path.write_text(json.dumps(new_durations))
